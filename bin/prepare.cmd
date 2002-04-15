4396be13c2ad81868b6259e1acdfc55683a990dc
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-04-15 22:04:17 +0000

    - replaced calls to move and unpack files with environment vars   to allow quick changes for testing purposes - changed calls for pkunzip2 to _move.cmd in order to allow   creation of target dir before unpacking, because pkunzip2   fails if no drive letter is given

--- bin/prepare.cmd
+++ bin/prepare.cmd
@@ -15,7 +15,7 @@
 :
 : Copyright (c) Netlabs EPM Distribution 2002
 :
-: $Id: prepare.cmd,v 1.1 2002-04-15 16:37:51 ktk Exp $
+: $Id: prepare.cmd,v 1.2 2002-04-15 22:04:17 cla Exp $
 :
 : ===========================================================================
 :
@@ -31,8 +31,8 @@
 : **************************************************************************/
 
  SETLOCAL
-
  SET PATH=bin;%PATH%
+ SET UNZ=CALL _UNPACK
 
  SET LOGFILE=%1
  SET BASEURL=%2
@@ -75,75 +75,73 @@
 
 : --- unpack package zip files
  ECHO - unpack EPM 6.03 packages
- pkunzip2 -o %ZIPSRCDIR%\epm603               -d %UNZIPPEDDIR%                 >>%LOGFILE% 2>&1
+ %UNZ% %ZIPSRCDIR%\epm603               %UNZIPPEDDIR%                          >>%LOGFILE% 2>&1
 
 : --- unpack main application package and distribute to subdirectories
  SET TARGET=%UNZIPPEDDIR%\EPMAPP
- pkunzip2 -o %UNZIPPEDDIR%\epmapp             -d %UNZIPPEDDIR%\EPMAPP\BIN      >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmapp             %UNZIPPEDDIR%\EPMAPP\BIN               >>%LOGFILE% 2>&1
 
  DEL %TARGET%\BIN\README.EPM                                                   >NUL 2>&1
  DEL %TARGET%\BIN\TTITALIC.BMP                                                 >NUL 2>&1
  DEL %TARGET%\BIN\EPMHELP.QHL                                                  >NUL 2>&1
 
  MD %TARGET%\BIN\BMP                                                           >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.BMP %TARGET%\BIN\BMP                                >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.BMP %TARGET%\BIN\BMP                                     >NUL 2>&1
 
  MD %TARGET%\BIN\KEYWORDS                                                      >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\EPMKWDS.* %TARGET%\BIN\KEYWORDS                       >NUL 2>&1
+ %MOV% %TARGET%\BIN\EPMKWDS.* %TARGET%\BIN\KEYWORDS                            >NUL 2>&1
 
  MD %TARGET%\BIN\EX                                                            >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.EX        %TARGET%\BIN\EX                           >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\ACTIONS.LST %TARGET%\BIN\EX                           >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.EX        %TARGET%\BIN\EX                                >NUL 2>&1
+ %MOV% %TARGET%\BIN\ACTIONS.LST %TARGET%\BIN\EX                                >NUL 2>&1
 
  MD %TARGET%\BIN\BAR                                                           >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.BAR %TARGET%\BIN\BAR                                >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.BAR %TARGET%\BIN\BAR                                     >NUL 2>&1
 
 : --- unpack further main application package components
 
- pkunzip2 -o %UNZIPPEDDIR%\epmdll  -d %UNZIPPEDDIR%\EPMDLL\DLL                 >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmhlp  -d %UNZIPPEDDIR%\EPMHLP\HELP                >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmbk   -d %UNZIPPEDDIR%\EPMBK\BOOK                 >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmbmps -d %UNZIPPEDDIR%\EPMBMPS\BIN\BMP            >>%LOGFILE% 2>&1
-
-:test
+ %UNZ% %UNZIPPEDDIR%\epmdll  %UNZIPPEDDIR%\EPMDLL\DLL                          >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmhlp  %UNZIPPEDDIR%\EPMHLP\HELP                         >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmbk   %UNZIPPEDDIR%\EPMBK\BOOK                          >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmbmps %UNZIPPEDDIR%\EPMBMPS\BIN\BMP                     >>%LOGFILE% 2>&1
 
 : --- unpack speech support package and distribute to subdirectories
 
  SET TARGET=%UNZIPPEDDIR%\EPMSPCH
- pkunzip2 -o %UNZIPPEDDIR%\epmspch -d %TARGET%\BIN                             >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmspch   %TARGET%\BIN                                    >>%LOGFILE% 2>&1
  DEL %TARGET%\BIN\README.TXT                                                   >NUL 2>&1
 
  MD %TARGET%\DLL                                                               >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.DLL %TARGET%\DLL                                    >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.DLL %TARGET%\DLL                                         >NUL 2>&1
 
  MD %TARGET%\BIN\MACROS                                                        >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.E   %TARGET%\BIN\MACROS                             >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.E   %TARGET%\BIN\MACROS                                  >NUL 2>&1
 
  DEL %TARGET%\BIN\*.551
  MD %TARGET%\BIN\EX                                                            >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.603 %TARGET%\BIN\EX\*.EX                            >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.603 %TARGET%\BIN\EX\*.EX                                 >NUL 2>&1
 
  MD %TARGET%\BIN\BMP                                                           >NUL 2>&1
- CALL _MOVE %TARGET%\BIN\*.BMP %TARGET%\BIN\BMP                                >NUL 2>&1
+ %MOV% %TARGET%\BIN\*.BMP %TARGET%\BIN\BMP                                     >NUL 2>&1
 
 : --- unpack macro packages and distribute to subdirectories
 
- pkunzip2 -o %UNZIPPEDDIR%\epmmac  -d %UNZIPPEDDIR%\EPMMAC\BIN\MACROS          >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmmac2 -d %UNZIPPEDDIR%\EPMMAC2\BIN\MACROS         >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmsmp  -d %UNZIPPEDDIR%\EPMSMP\MACROS\SAMPLES      >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmasi  -d %UNZIPPEDDIR%\EPMASI\BIN\MACROS\MYASSIST >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\lampdq  -d %UNZIPPEDDIR%\LAMPDQ\BIN\MACROS\LAMPDQ   >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmmac  %UNZIPPEDDIR%\EPMMAC\BIN\MACROS                   >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmmac2 %UNZIPPEDDIR%\EPMMAC2\BIN\MACROS                  >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmsmp  %UNZIPPEDDIR%\EPMSMP\MACROS\SAMPLES               >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmasi  %UNZIPPEDDIR%\EPMASI\BIN\MACROS\MYASSIST          >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\lampdq  %UNZIPPEDDIR%\LAMPDQ\BIN\MACROS\LAMPDQ            >>%LOGFILE% 2>&1
 
- pkunzip2 -o %UNZIPPEDDIR%\ebooke  -d %UNZIPPEDDIR%\EBOOKE\BIN\MACROS\EBOOKE   >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\ebooke  %UNZIPPEDDIR%\EBOOKE\BIN\MACROS\EBOOKE            >>%LOGFILE% 2>&1
  DEL %UNZIPPEDDIR%\EBOOKE\BIN\MACROS\EBOOKE\READ.ME
 
 : --- unpack sample packages and distribute to subdirectories
 
- pkunzip2 -o %UNZIPPEDDIR%\epmatr   -d %UNZIPPEDDIR%\EPMATR\SAMPLES\ATTR       >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmdde   -d %UNZIPPEDDIR%\EPMDDE\SAMPLES\DDE        >>%LOGFILE% 2>&1
- pkunzip2 -o %UNZIPPEDDIR%\epmrex   -d %UNZIPPEDDIR%\EPMREX\SAMPLES\REXX       >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmatr   %UNZIPPEDDIR%\EPMATR\SAMPLES\ATTR                >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmdde   %UNZIPPEDDIR%\EPMDDE\SAMPLES\DDE                 >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmrex   %UNZIPPEDDIR%\EPMREX\SAMPLES\REXX                >>%LOGFILE% 2>&1
 
- pkunzip2 -o %UNZIPPEDDIR%\epmcsamp -d %UNZIPPEDDIR%\EPMCSAMP\SAMPLES          >>%LOGFILE% 2>&1
+ %UNZ% %UNZIPPEDDIR%\epmcsamp %UNZIPPEDDIR%\EPMCSAMP\SAMPLES                   >>%LOGFILE% 2>&1
 
  REN %UNZIPPEDDIR%\EPMCSAMP\SAMPLES\EPMCSAMP C                                 >>%LOGFILE% 2>&1
 
@@ -154,7 +152,7 @@
 : --- apply update
 
  ECHO - unpack EPM 6.03b update files
- unzip -o  %ZIPSRCDIR%\epm603b   -d %UNZIPPEDDIR%\update                       >>%LOGFILE% 2>&1
+ unzip -o  %ZIPSRCDIR%\epm603b -d %UNZIPPEDDIR%\update                         >>%LOGFILE% 2>&1
 
  ECHO - updating package
  replace %UNZIPPEDDIR%\update\* %UNZIPPEDDIR% /U /S                            >%UNZIPPEDDIR%\update.log 2>&1
