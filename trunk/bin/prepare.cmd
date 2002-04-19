60726662101824ef1a41de6daf0a47a944c8434f
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-04-19 19:59:16 +0000

    - added code to catch errorlevel > 1 - removed dummy code to check for SYS errors after unpack

--- trunk/bin/prepare.cmd
+++ trunk/bin/prepare.cmd
@@ -15,7 +15,7 @@
 :
 : Copyright (c) Netlabs EPM Distribution 2002
 :
-: $Id: prepare.cmd,v 1.11 2002-04-18 17:01:36 cla Exp $
+: $Id: prepare.cmd,v 1.12 2002-04-19 19:59:16 cla Exp $
 :
 : ===========================================================================
 :
@@ -64,91 +64,139 @@
 :geterror
  ECHO.
  ECHO Error: could not wget %CHECK%...
- PAUSE
  GOTO end
 
 
 : ---------- unpack files and apply update
 :unpack
 
- IF EXIST %LOGFILE%  DEL %LOGFILE%                                             >NUL 2>&1
+: --- exit with errors
 
+ SET CHECKERROR=IF ERRORLEVEL 2 GOTO end
+
+: --- cleanup directory and logfile
+
+ ECHO - cleanup directory %UNZIPPEDDIR%
  IF EXIST %UNZIPPEDDIR% CALL KD %UNZIPPEDDIR%                                  >NUL 2>&1
  MD %UNZIPPEDDIR% >NUL 2>&1
+
+ IF EXIST %LOGFILE%  DEL %LOGFILE%                                             >NUL 2>&1
 
 : --- unpack package zip files
  ECHO - unpack EPM 6.03 packages
  %UNZ% %ZIPSRCDIR%\epm603               %UNZIPPEDDIR%                          >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
 : --- unpack main application package and distribute to subdirectories
  SET TARGET=%UNZIPPEDDIR%\EPMAPP
  %UNZ% %UNZIPPEDDIR%\epmapp             %UNZIPPEDDIR%\EPMAPP\BIN               >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  DEL %TARGET%\BIN\README.EPM                                                   >>%LOGFILE% 2>&1
+ %CHECKERROR%
  DEL %TARGET%\BIN\TTITALIC.BMP                                                 >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\BMP                                                           >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.BMP %TARGET%\BIN\BMP                                     >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\KEYWORDS                                                      >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\EPMKWDS.* %TARGET%\BIN\KEYWORDS                            >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\EX                                                            >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.EX        %TARGET%\BIN\EX                                >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\ACTIONS.LST %TARGET%\BIN\EX                                >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\BAR                                                           >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.BAR %TARGET%\BIN\BAR                                     >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\NDX                                                           >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.NDX %TARGET%\BIN\NDX                                     >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
 : --- unpack further main application package components
 
  %UNZ% %UNZIPPEDDIR%\epmdll  %UNZIPPEDDIR%\EPMDLL\DLL                          >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmhlp  %UNZIPPEDDIR%\EPMHLP\HELP                         >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmbk   %UNZIPPEDDIR%\EPMBK\BOOK                          >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmbmps %UNZIPPEDDIR%\EPMBMPS\BIN\BMP                     >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
 : --- unpack speech support package and distribute to subdirectories
 
  SET TARGET=%UNZIPPEDDIR%\EPMSPCH
  %UNZ% %UNZIPPEDDIR%\epmspch   %TARGET%\BIN                                    >>%LOGFILE% 2>&1
+ %CHECKERROR%
  DEL %TARGET%\BIN\README.TXT                                                   >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\DLL                                                               >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.DLL %TARGET%\DLL                                         >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\MACROS                                                        >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.E   %TARGET%\BIN\MACROS                                  >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  DEL %TARGET%\BIN\*.551                                                        >>%LOGFILE% 2>&1
+ %CHECKERROR%
  MD %TARGET%\BIN\EX                                                            >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.603 %TARGET%\BIN\EX\*.EX                                 >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  MD %TARGET%\BIN\BMP                                                           >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %MOV% %TARGET%\BIN\*.BMP %TARGET%\BIN\BMP                                     >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
 : --- unpack macro packages and distribute to subdirectories
 
  %UNZ% %UNZIPPEDDIR%\epmmac  %UNZIPPEDDIR%\EPMMAC\BIN\MACROS                   >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmmac2 %UNZIPPEDDIR%\EPMMAC2\BIN\MACROS                  >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmsmp  %UNZIPPEDDIR%\EPMSMP\BIN\MACROS\SAMPLES           >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmatr   %UNZIPPEDDIR%\EPMATR\BIN\MACROS\ATTR             >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmasi  %UNZIPPEDDIR%\EPMASI\BIN\MACROS\MYASSIST          >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\lampdq  %UNZIPPEDDIR%\LAMPDQ\BIN\MACROS\LAMPDQ            >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  %UNZ% %UNZIPPEDDIR%\ebooke  %UNZIPPEDDIR%\EBOOKE\BIN\MACROS\EBOOKE            >>%LOGFILE% 2>&1
+ %CHECKERROR%
  DEL %UNZIPPEDDIR%\EBOOKE\BIN\MACROS\EBOOKE\READ.ME
+ %CHECKERROR%
 
 : --- unpack sample packages and distribute to subdirectories
 
  %UNZ% %UNZIPPEDDIR%\epmdde   %UNZIPPEDDIR%\EPMDDE\SAMPLES\DDE                 >>%LOGFILE% 2>&1
+ %CHECKERROR%
  %UNZ% %UNZIPPEDDIR%\epmrex   %UNZIPPEDDIR%\EPMREX\SAMPLES\REXX                >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  %UNZ% %UNZIPPEDDIR%\epmcsamp %UNZIPPEDDIR%\EPMCSAMP\SAMPLES                   >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
  REN %UNZIPPEDDIR%\EPMCSAMP\SAMPLES\EPMCSAMP C                                 >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
 : --- cleanup here
 
@@ -158,23 +206,13 @@
 
  ECHO - unpack EPM 6.03b update files
  unzip -o  %ZIPSRCDIR%\epm603b -d %UNZIPPEDDIR%\update                         >>%LOGFILE% 2>&1
+ %CHECKERROR%
 
- ECHO - updating package
+ ECHO - apply update
  replace %UNZIPPEDDIR%\update\* %UNZIPPEDDIR% /U /S                            >%UNZIPPEDDIR%\update.log 2>&1
 
-
-: --- remove R/O atttributes
+: --- remove R/O atttributes on all files
 
  ATTRIB -r %UNZIPPEDDIR%\* /S
 
-: --- now check for errors in logfile
-:
-:grep SYS....: %LOGFILE%
-:IF ERRORLEVEL 1 GOTO end
-:
-:ECHO.
-:ECHO errors occurred. Press Ctrl-Break to abort or any other key to continue...
-:PAUSE > NUL
-
 :end
-
