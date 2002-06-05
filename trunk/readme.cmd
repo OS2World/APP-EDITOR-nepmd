4b666d6fa24f4efb07f4d459e46670f7e1d42aa9
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-06-05 22:22:43 +0000

    - changed call to makefile.inf to not using the makefile to allow   reading also when makefile processing aborts due to a missing program

--- trunk/readme.cmd
+++ trunk/readme.cmd
@@ -1,3 +1,3 @@
-: $Id: readme.cmd,v 1.1 2002-04-20 14:50:38 cla Exp $
+: $Id: readme.cmd,v 1.2 2002-06-05 22:22:43 cla Exp $
 @ECHO OFF
- nmake  /NOLOGO
+ @start view bin\makefile %1 %2 %3 %4 %5 %6 %7 %8 %9
