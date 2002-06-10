3b1fa5848aec9dbb199701005bf3bdcb5b34d665
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-06-10 10:32:57 +0000

    - implemented new bullet in GUI for loading the logfile

--- trunk/src/gui/recomp/job.h
+++ trunk/src/gui/recomp/job.h
@@ -6,7 +6,7 @@
 *
 * Copyright (c) Netlabs EPM Distribution Project 2002
 *
-* $Id: job.h,v 1.3 2002-06-08 23:40:50 cla Exp $
+* $Id: job.h,v 1.4 2002-06-10 10:32:56 cla Exp $
 *
 * ===========================================================================
 *
@@ -28,7 +28,7 @@
 #define WM_USER_UPDATE_JOBMACHINE    (WM_USER + 0x2000)
 #define UPDATE_JOB_STATUS            WinPostMsg( hwnd, WM_USER_UPDATE_JOBMACHINE, 0, 0)
 #define UPDATE_JOB_STATUS_DATA(h, p) WinPostMsg( hwnd, WM_USER_UPDATE_JOBMACHINE, MPFROMLONG(h), MPFROMP(p))
-#define ABORT_JOB                    { pwd->ulJobStatus = JOB_ACTION_FINISH; UPDATE_JOB_STATUS;}
+#define ABORT_JOB                    { pwd->ulJobStatus = JOB_ACTION_LOADLOG; UPDATE_JOB_STATUS;}
 
 
 // job status
@@ -38,7 +38,8 @@
 #define JOB_ACTION_RECOMPILE_EPM             3
 #define JOB_ACTION_CLOSE_EPMWINDOWS          4
 #define JOB_ACTION_RELOAD_FILES              5
-#define JOB_ACTION_FINISH                    6
+#define JOB_ACTION_LOADLOG                   6
+#define JOB_ACTION_FINISH                    7
 
 #define JOB_STATUS_INITIALIZING            100
 #define JOB_STATUS_LINKING_MACRO           101
@@ -46,7 +47,8 @@
 #define JOB_STATUS_RECOMPILING_EPM         103
 #define JOB_STATUS_CLOSING_EPMWINDOWS      104
 #define JOB_STATUS_RELOADING_FILES         105
-#define JOB_STATUS_DONE                    106
+#define JOB_STATUS_LOADINGLOG              106
+#define JOB_STATUS_DONE                    107
 
 
 #define RESET_STATUS                (pwd->ulJobStatus = JOB_ACTION_INITIALIZE)
