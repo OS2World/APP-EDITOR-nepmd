95001a8f57a2ba4dd0494033ca02ae028ff2734f
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-06-11 11:08:24 +0000

    *** empty log message ***

--- trunk/readme.cmd
+++ trunk/readme.cmd
@@ -1,3 +1,29 @@
-: $Id: readme.cmd,v 1.2 2002-06-05 22:22:43 cla Exp $
 @ECHO OFF
- @start view bin\makefile %1 %2 %3 %4 %5 %6 %7 %8 %9
+: ***************************** Module Header ******************************\
+:
+: Module Name:
+:
+: Copyright (c) Netlabs EPM Distribution Project 2002
+:
+: $Id: readme.cmd,v 1.3 2002-06-11 11:08:24 cla Exp $
+:
+: ===========================================================================
+:
+: This file is part of the Netlabs EPM Distribution package and is free
+: software.  You can redistribute it and/or modify it under the terms of the
+: GNU General Public License as published by the Free Software
+: Foundation, in version 2 as it comes in the "COPYING" file of the
+: Netlabs EPM Distribution.  This library is distributed in the hope that it
+: will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+: of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+: General Public License for more details.
+:
+: **************************************************************************/
+
+ SETLOCAL
+
+ SET PARMS=%1 %2 %3 %4 %5 %6 %7 %8 %9
+ IF .%PARMS% == . SET PARMS=Making
+
+ start view bin\makefile %PARMS%
+
