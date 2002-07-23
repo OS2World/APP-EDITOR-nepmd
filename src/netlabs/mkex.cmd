58bf2dfa9d7ac7008f51907fea37be31e96daceb
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-07-23 11:40:48 +0000

    First revision

---
+++ src/netlabs/mkex.cmd
@@ -0,0 +1,46 @@
+/****************************** Module Header *******************************
+*
+* Module Name: mkex.cmd
+*
+* Syntax: mkex target_dir
+*
+* Script for to create the NEPMD version of EPM.EX
+* 
+* As a precaution EPMPATH is set to the macros directory only in order not
+* to use any of source files from other directories
+*
+* Copyright (c) Netlabs EPM Distribution Project 2002
+*
+* $Id: mkex.cmd,v 1.1 2002-07-23 11:40:48 cla Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+
+ rcx = SETLOCAL();
+
+ /* set include path for macro compiler */
+ 'SET EPMPATH=macros';
+
+ /* get parms */
+ PARSE ARG TargetDir;
+ TargetDir = STRIP( TargetDir);
+ IF (TargetDir = '') THEN
+ DO
+    SAY 'mkex: error: target directory not specified.';
+    EXIT( 87); /* ERROR.INVALID_PARAMETER */
+ END;
+
+ /* call compiler */
+ 'etpm epm.e' TargetDir'\epm.ex';
+
+ EXIT (rc);
