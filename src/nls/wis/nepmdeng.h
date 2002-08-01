31a1bac2878f516645c90e28be27cf2790cc0438
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-08-01 13:21:17 +0000

    First revision

---
+++ src/nls/wis/nepmdeng.h
@@ -0,0 +1,24 @@
+
+#define BUTTON_NEXT "~Next"
+
+#define PCK_1_TITLE "Base Application Support"
+#define PCK_2_TITLE "Recompilation package"
+#define PCK_3_TITLE "Programming samples"
+#define PCK_4_TITLE "Speech Support"
+#define PCK_99_TITLE "EPMBBS Base Application"
+
+
+#define TITLE_NEPMD_FOLDER                       "EPM v6.03b"
+#define TITLE_NEPMD_EXECUTE                      "EPM v6.03b a"
+#define TITLE_NEPMD_VIEW_EPMTECH                 "EPM Technical Reference"
+#define TITLE_NEPMD_VIEW_EPMUSERS                "EPM Users Guide"
+#define TITLE_NEPMD_VIEW_NEPMD                   "OS/2 Netlabs^EPM Distribution"
+#define TITLE_NEPMD_RECOMP                       "Recompile EPM"
+#define TITLE_NEPMD_NEPMD_SAMPLES_FOLDER         "EPM Programming Samples"
+#define TITLE_NEPMD_NEPMD_VIEW_EPMTK             "Programmers Guide to the EPM Toolkit"
+#define TITLE_NEPMD_NEPMD_VIEW_EPMCM_README      "EPM C macro Readme"
+#define TITLE_NEPMD_NEPMD_VIEW_EPMRM_README      "EPM REXX macro Readme"
+#define TITLE_NEPMD_NEPMD_SPEECH                 "Start EPM Speech Support"
+#define TITLE_NEPMD_NEPMD_SPEECH_SHADOW          "Start EPM Speech Support"
+#define TITLE_NEPMD_NEPMD_WPSTART_SHADOW         "Startup Folder Shadow"
+
