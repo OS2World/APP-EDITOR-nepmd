7e3b82bcfca9e7898d550f81b94d37fa23439cba
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-08-10 12:34:06 +0000

    First revision

---
+++ bin/setinit.cmd
@@ -0,0 +1,56 @@
+/****************************** Module Header *******************************
+*
+* Module Name: setinit.cmd
+*
+*   Syntax:  setinit
+*
+*   This program sets initialization data to NEPMD.INI, just as if
+*   NEPMD was installed in the DEBUG directory. This way all programs
+*   in DEBUG\NETLABS\BIN will think they are running on a true installed
+*   NEPMD version
+*
+*   NOTE: Running this script will of course corrupt any true NEPMD
+*         installation !!! To make a true installation work again,
+*         you will have to reinstall the base NEPMD package again
+*         to restore the INI vars to it.
+*
+* Copyright (c) Netlabs EPM Distribution Project 2002
+*
+* $Id: setinit.cmd,v 1.1 2002-08-10 12:34:06 cla Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+
+ /* load REXX util */
+ call RxFuncAdd    'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs'
+ call SysLoadFuncs
+
+ /* defaults */
+ IniApp         = 'NEPMD';
+ IniKeyPath     = 'Path';
+
+ /* determine call directory */
+ PARSE SOURCE . . CallName;
+ CallDir = LEFT( CallName, LASTPOS( '\', CallName) - 1);
+ BaseDir = LEFT( CallDir,  LASTPOS( '\', CallDir)  - 1);
+ InstDir = BaseDir'\debug';
+
+ /* display the vaule just set */
+ rc = SysIni(, IniApp, IniKeyPath, InstDir);
+ IF (rc = 'ERROR:') THEN
+    CurrentPath = '<not defined>';
+ ELSE
+    CurrentPath = SysIni(, IniApp, IniKeyPath);
+
+ SAY 'Install path is set to:' CurrentPath;
+
