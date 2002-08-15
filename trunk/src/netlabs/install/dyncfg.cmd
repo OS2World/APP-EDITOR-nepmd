9cebc1cb604a595ecc958d60d70c99bf72bfacd6
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-08-15 13:23:27 +0000

    - implemented proper error message handling for the PM EXE versions of   nlsetup.cmd and nlsetup.cmd, dyncfg.cmd as currently the only called   cmd using it

--- trunk/src/netlabs/install/dyncfg.cmd
+++ trunk/src/netlabs/install/dyncfg.cmd
@@ -18,7 +18,7 @@
 *
 * Copyright (c) Netlabs EPM Distribution Project 2002
 *
-* $Id: dyncfg.cmd,v 1.3 2002-08-12 15:06:07 cla Exp $
+* $Id: dyncfg.cmd,v 1.4 2002-08-15 13:23:27 cla Exp $
 *
 * ===========================================================================
 *
@@ -37,6 +37,7 @@
  env   = 'OS2ENVIRONMENT';
  TRUE  = (1 = 1);
  FALSE = (0 = 1);
+ CrLf  = '0d0a'x
  Redirection = '>NUL 2>&1';
  GlobalVars = 'env TRUE FALSE Redirection';
 
@@ -45,7 +46,9 @@
 
  /* defaults */
  rc = 0;
- EaName = 'NEPMD.Loader';
+ LoaderEaName = 'NEPMD.Loader';
+ ErrorQueueName = VALUE( 'NEPMD_RXQUEUE', , env);
+ ErrorMessage = '';
 
  DO UNTIL (TRUE)
 
@@ -60,11 +63,12 @@
     IF (Parm = 'DEINSTALL') THEN
     DO
        /* delete EPM.EXE in ?:\os2 if it is ours */
-       IF ((fCheckFileExists) & (IsNepmdExecutable( CheckFile))) THEN
+       IF ((fCheckFileExists) & (IsNepmdExecutable( CheckFile, LoaderEaName))) THEN
        DO
           rc = SysFileDelete( CheckFile);
           LEAVE;
        END;
+       LEAVE;
     END;
 
     /* get the base directory of the NEPMD installation */
@@ -72,29 +76,32 @@
     PARSE VALUE SysIni(, 'NEPMD', 'Path') WITH InstallPath'00'x;
     IF (InstallPath = 'ERROR:') THEN
     DO
-       SAY 'error: NEPMD configuration not found.';
+       ErrorMessage = 'error: NEPMD configuration not found.';
        rc = 3; /* ERROR_PATH_NOT_FOUND */
        LEAVE;
     END;
 
-   
+
     /* determine name of loader executable */
     LoaderExe = InstallPath'\netlabs\bin\epm.exe';
     IF (\FileExist( LoaderExe)) THEN
     DO
-       SAY 'error:' LoaderExe 'not found, NEPMD installation is not complete.';
+       ErrorMessage = 'error:' LoaderExe 'not found, NEPMD installation is not complete.';
        rc = 2; /* ERROR_FILE_NOT_FOUND */
        LEAVE;
     END;
-   
+
     /* don't touch any EPM.EXE not being ours here */
-    IF ((fCheckFileExists) & (\IsNepmdExecutable( CheckFile))) THEN
+    IF ((fCheckFileExists) & (\IsNepmdExecutable( CheckFile, LoaderEaName))) THEN
     DO
-       SAY 'error: ' CheckFile 'is not of NEPMD, cannot continue.';
+       ErrorMessage = 'error: ' CheckFile 'is not of NEPMD, cannot continue.',
+                      'Dynamic EPM configuration and with it the NEPMD extensions will not properly work !'CrLf''CrLf||,
+                      'Remove this file from this directory (usually it',
+                      'should rather be installed in ' BootDrive'\OS2\APPS) and repeat the installation!';
        rc = 5; /* ERROR_ACCESS_DENIED */
        LEAVE;
     END;
-   
+
     /* determine original EPM.EXE along the path */
     PathList = VALUE( 'PATH',,env);
     fOs2DirPassed = FALSE;
@@ -102,14 +109,14 @@
     DO WHILE (PathList \= '')
        PARSE VAR PathList ThisDir';'PathList;
        IF (ThisDir = '') THEN ITERATE;
-   
+
        /* is it the OS/2 directory ? */
        IF (TRANSLATE( ThisDir) = OS2Dir) THEN
        DO
           fOs2DirPassed = TRUE;
           ITERATE;
        END;
-   
+
        /* now check for EPM */
        IF (RIGHT( ThisDir, 1) \= '\') THEN
           ThisDir = ThisDir'\';
@@ -117,39 +124,57 @@
        IF (FileExist( EpmExecutable)) THEN
           fEpmFound = TRUE;
     END;
-   
+
     IF (fEpmFound) THEN
     DO
        /* if os2 directory was not placed before, our loader will not be used */
        IF (\fOs2DirPassed) THEN
        DO
-          SAY 'error: EPM.EXE found in directory prior to' OS2Dir', cannot proceed.';
+          ErrorMessage = 'error: EPM.EXE found in directory prior to' OS2Dir', cannot proceed.';
           rc = 5; /* ERROR_ACCESS_DENIED */
           LEAVE;
        END;
-   
+
        /* copy EPM.EXE of NEPMD */
        'COPY' LoaderExe OS2Dir Redirection;
        IF (rc \= 0) THEN
        DO
-          SAY 'error: cannot write' CheckFile'!';
+          ErrorMessage = 'error: cannot write' CheckFile'!';
           rc = 5; /* ERROR_ACCESS_DENIED */
           LEAVE;
        END;
-   
+
        /* mark EXE with special attribute (EAT_STRING) */
        LoaderInfo = '1';
        EaLen = REVERSE( RIGHT( D2C( LENGTH( LoaderInfo)), 2, D2C(0)));
        EaValue = 'FDFF'x''EaLen''LoaderInfo;
-       rcx = SysPutEa( CheckFile, NepmdEaName, EaValue);
-   
+       rcx = SysPutEa( CheckFile, LoaderEaName, EaValue);
+
     END;
  END;
 
- IF (rc \= 0) THEN
-    'PAUSE';
+ /* report error message */
+ SELECT
+    /* no error here */
+    WHEN (rc = 0) THEN NOP;
 
- EXIT( 0);
+    /* called by frame program: insert error */
+    /* message into standard REXX queue     */
+    WHEN (ErrorQueueName \= '') THEN
+    DO
+       rcx = RXQUEUE( 'SET', ErrorQueueName);
+       PUSH ErrorMessage;
+    END;
+
+    /* called directly, method */
+    OTHERWISE
+    DO
+       SAY ErrorMessage;
+       'PAUSE';
+    END;
+ END;
+
+ EXIT( rc);
 
 /* ------------------------------------------------------------------------- */
 FileExist: PROCEDURE
@@ -159,7 +184,7 @@ FileExist: PROCEDURE
 
 /* ========================================================================= */
 IsNepmdExecutable: PROCEDURE EXPOSE (GlobalVars)
- PARSE ARG CheckFile;
+ PARSE ARG CheckFile, EaName;
 
  fFound = FALSE;
 
@@ -169,7 +194,7 @@ IsNepmdExecutable: PROCEDURE EXPOSE (GlobalVars)
     IF (FileExist( CheckFile)) THEN
     DO
        /* is it our executable ? */
-       rc = SysGetEa( CheckFile, NepmdEaName, LoaderTag);
+       rc = SysGetEa( CheckFile, EaName, LoaderTag);
        IF ((rc = 0) & (LoaderTag \= '')) THEN
           fFound = 1;
     END;
