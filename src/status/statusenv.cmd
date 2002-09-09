b41e20efbafe0b0288c2d0c3cfb2f95e78d722ad
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-09-09 20:56:37 +0000

    First revision

---
+++ src/status/statusenv.cmd
@@ -0,0 +1,27 @@
+@ECHO OFF
+: ***************************** Module Header ******************************\
+:
+: Module Name:
+:
+: Copyright (c) Netlabs EPM Distribution Project 2002
+:
+: $Id: statusenv.cmd,v 1.1 2002-09-09 20:56:37 cla Exp $
+:
+: ===========================================================================
+:
+: This file is part of the Netlabs EPM Distribution package and is free
+: software.  You can redistribute it and/or modify it under the terms of the
+: GNU General Public License as published by the Free Software
+: Foundation, in version 2 as it comes in the "COPYING" file of the
+: Netlabs EPM Distribution.  This library is distributed in the hope that it
+: will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+: of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+: General Public License for more details.
+:
+: **************************************************************************/
+
+ SET APACHE_STEM=NEPMD-Status
+ SET APACHE_PORT=55555
+ SET APACHE_DOCROOT=.
+ SET APACHE_SERVERNAME=localhost
+
