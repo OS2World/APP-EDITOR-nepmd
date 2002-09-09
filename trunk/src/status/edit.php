a1210b2b80b3150cc6babc23a52e4c0f40db2865
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-09-09 15:37:37 +0000

    - moved code out to allow usage by new file new.php for to use the code   to create a new entry file   - moved function addoptions() to new file formrt.inc   - moved HTML form code to new file formcode.inc

--- trunk/src/status/edit.php
+++ trunk/src/status/edit.php
@@ -6,7 +6,7 @@
 *
 * Copyright (c) Netlabs EPM Distribution Project 2002
 *
-* $Id: edit.php,v 1.5 2002-07-21 22:42:10 cla Exp $
+* $Id: edit.php,v 1.6 2002-09-09 15:37:37 cla Exp $
 *
 * ===========================================================================
 *
@@ -22,37 +22,9 @@
 *************************************************************************/ ?>
 
 <? include( "filedb.inc") ?>
-
-<html>
-<body text="#000000" bgcolor=#FFFFFF link=#CC6633 vlink=#993300 alink=#6666CC>
+<? include( "formrt.inc") ?>
 
 <?
-function addoptions(  $default, $optionfile)
-{
-// read all options from file
-$aoptions = file( $optionfile);
-
-// search default in options
-$default_upper = strtoupper( trim($default));
-for ($i = 0; $i < count( $aoptions); $i++)
-   {
-   // read entry, ignore empty lines and comments
-   $option = trim( $aoptions[ $i]);
-   if (strlen( $option) == 0)
-      continue;
-   if (strlen( strpos( $option, ":")) > 0)
-      continue;
-
-   // ckeck if option should be selected
-   $option_upper = strtoupper( trim( $option));
-   $selected = (strcmp( $option_upper, $default_upper) == 0);
-   if ($selected)
-      echo "<option selected>".$option;
-   else
-      echo "<option>".$option;
-   }
-}
-
 // load file
 $file = $_GET[ "file"];
 if ($file != "")
@@ -81,75 +53,11 @@ if ($file != "")
          }
       }
 
-   // HTML form
-   echo "<form action=\"update.php\" name=\"EditDbFile\" method=\"post\" enctype=\"text/plain\">";
-   echo "<table width=90% border=0>";
-   echo "<input name=file type=hidden value=\"".$file."\">";
-
-   echo "<tr><td align=right valign=center bgcolor=#dddddd>";
-   echo "file:";
-   echo "</td><td valign=center bgcolor=#dddddd>";
-   echo $file;
-   echo "</td></tr>";
-
-   echo "<tr><td align=right>";
-   echo "title:";
-   echo "</td><td>";
-   echo "<input name=title size=70 maxlength=256 value=\"".$title."\">";
-   echo "</td></tr>";
-
-   echo "<tr><td align=right bgcolor=#dddddd><font size=-1>";
-   echo "category:";
-   echo "</font></td><td bgcolor=#dddddd><font size=-1>";
-   echo "<select name=category size=1>";
-   addoptions( $category, "category.lst");
-   echo "</select>";
-
-   echo " prio: ";
-   echo "<select name=prio size=1>";
-   addoptions( $prio, "prio.lst");
-   echo "</select>";
-
-   echo " status: ";
-   echo "<select name=status size=1>";
-   addoptions( $status, "status.lst");
-   echo "</select>";
-
-
-   echo " files: ";
-   echo "<input name=filelist size=60 maxlength=256 value=\"".trim( $filelist)."\">";
-   echo "</font></td></tr>";
-
-   echo "<input name=updated type=hidden value=\"".$updated."\">";
-
-   echo "<tr><td valign=top align=right><font size=-1>";
-   echo "details:";
-   echo "</font></td><td><font size=-1>";
-   echo "<textarea name=details rows=6 cols=100>";
-   echo $details;
-   echo "</textarea>";
-   echo "</font></td></tr>";
-
-   echo "<tr><td valign=top align=right bgcolor=#dddddd><font size=-1>";
-   echo "commit comment:";
-   echo "</font></td><td bgcolor=#dddddd><font size=-1>";
-   echo "<textarea name=comment rows=2 cols=100>";
-   echo $comment;
-   echo "</textarea>";
-   echo "</font></td></tr>";
-
-   echo "<tr><td>";
-   echo "</td><td><font size=-1>";
-   echo "<input type=submit value=\"Apply\">";
-   echo "&nbsp;";
-   echo "<input type=reset value=\"Undo\">";
-   echo "&nbsp;";
-   echo "<input type=button value=\"Cancel\" onClick=\"self.location.href='details.php?file=".$file."'\">";
-   echo "</font></td></tr>";
-
-   echo "</table>";
+   // include HTML form for editing an existing file
+   // but comment may result out of an uncommitted add operation
+   $newfile = ($comment == "First revision");
+   include( "formcode.inc");
    }
 
 ?>
-</body>
-</html>
+
