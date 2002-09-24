def5be375015b53636656b0877d19dacb27c95bc
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-09-24 16:52:54 +0000

    First revision

---
+++ src/gui/test/dd.cmd
@@ -0,0 +1,28 @@
+@ECHO OFF
+: ***************************** Module Header ******************************\
+:
+: Module Name: dd.cmd
+:
+: This batch file starts the debugger and reads the current testcase to use
+: from env.cmd
+:
+: Copyright (c) Netlabs EPM Distribution Project 2002
+:
+: $Id: dd.cmd,v 1.1 2002-09-24 16:52:54 cla Exp $
+:
+: ===========================================================================
+:
+: This file is part of the Netlabs EPM Distribution package and is free
+: software.  You can redistribute it and/or modify it under the terms of the
+: GNU General Public License as published by the Free Software
+: Foundation, in version 2 as it comes in the "COPYING" file of the
+: Netlabs EPM Distribution.  This library is distributed in the hope that it
+: will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+: of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+: General Public License for more details.
+:
+: **************************************************************************/
+
+ call env.cmd
+ d %TESTCASE%
+
