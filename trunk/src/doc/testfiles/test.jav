20c5a915fb56dbad6c5432b84b22e7502ff61666
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-10-07 16:01:48 +0000

    First revision

---
+++ trunk/src/doc/testfiles/test.jav
@@ -0,0 +1,23 @@
+
+Comment - DEFAULT LIGHTMAGENTA
+==============================
+/* this is a comment */
+// this is a comment
+
+Literal - DEFAULT GREEN
+==========================
+"is a literal"
+
+
+character - DEFAULT LIGHTGREEN
+==============================
+'this is a character'
+
+special characters - DEFAULT LIGHTRED
+=====================================
+{ } ; , ?
+
+C keywords - DEFAULT MAGENTA
+============================
+class synchronized try
+
---
+++ trunk/src/doc/testfiles/test.pl
@@ -0,0 +1,22 @@
+
+Comment - DEFAULT LIGHTMAGENTA
+==============================
+/* this is a comment */
+# this is a comment
+
+Literal - DEFAULT GREEN
+==========================
+"is a literal"
+
+character - DEFAULT LIGHTGREEN
+============================
+'this is a character' `this is a character`
+
+special characters - DEFAULT LIGHTRED
+=====================================
+{ } ; , ?
+
+perl keywords - DEFAULT MAGENTA
+================================
+atan2 gethostbyname length package
+
