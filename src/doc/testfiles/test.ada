e7df8806646207e299b7a8e1b17975dff7d1ab02
cla <cla@72c63837-6175-e011-9ffd-00145e329d72>
2002-10-07 16:01:48 +0000

    First revision

---
+++ src/doc/testfiles/test.ada
@@ -0,0 +1,13 @@
+
+Comment - DEFAULT LIGHTMAGENTA
+==============================
+-- this is a comment
+
+Literal - DEFAULT LIGHTRED
+==========================
+"is a literal"   %this is a literal%
+
+Ada keywords - DEFAULT MAGENTA
+==============================
+abort abs abstract accept access aliased
+
---
+++ src/doc/testfiles/test.cmd
@@ -0,0 +1,28 @@
+/* this comment to let NEPMD recognize this as a REXX script
+
+Comment - YELLOW BLACK
+======================
+/* this is a comment */
+
+Literal - DEFAULT GREEN
+==========================
+"is a literal"
+
+character - DEFAULT LIGHTGREEN
+============================
+'this is a character'
+
+special characters - DEFAULT LIGHTRED
+=====================================
+( ) ; : = * /
+
+REXX keywords - DEFAULT LIGHTRED
+================================
+nop numeric options pull
+
+OS/2 and SysRexxFunctions - DEFAULT LIGHTMAGENTA
+================================================
+RxFuncQuery SysDriveMap setlocal endlocal filespec
+
+*/
+
