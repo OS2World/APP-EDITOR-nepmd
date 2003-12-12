ef0cf9268923bcb51fc4734fe6e5776e1bd68b63
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2003-12-12 20:17:08 +0000

    - Added missing header.

--- src/netlabs/bin/epmchgpal.cmd
+++ src/netlabs/bin/epmchgpal.cmd
@@ -1,3 +1,26 @@
+/****************************** Module Header *******************************
+*
+* Module Name: epmchgpal.cmd
+*
+* Change EPM''s standard 16-color palette for keyword hiliting
+*
+* Copyright (c) Netlabs EPM Distribution Project 2003
+*
+* $Id: epmchgpal.cmd,v 1.2 2003-12-12 20:17:08 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+
 /* REXX */
 
 call RxFuncAdd 'SysLoadFuncs', 'REXXUTIL', 'SysLoadFuncs'
