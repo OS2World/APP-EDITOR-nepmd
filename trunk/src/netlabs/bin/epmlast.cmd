4b318138d75b3581cbb5334294cedfe7ffa30a74
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2005-11-23 22:31:04 +0000

    First revision

---
+++ trunk/src/netlabs/bin/epmlast.cmd
@@ -0,0 +1,72 @@
+/****************************** Module Header *******************************
+*
+* Module Name: epmlast.cmd
+*
+* Syntax: epmlast [<epm_exe> [<epm_args>]]
+*
+* <epm_exe> and/or <epm_args> both can be enclosed with " chars.
+*
+* Helper batch for to open a new EPM window with the last saved ring.
+*
+* This program is called by EPM's Restart command in order to close the
+* EPM window properly before a new one is restarted. Therefore the new EPM
+* window is started delayed.
+*
+* If called without parameters, the delay will be deactivated. Therefore
+* this program can be used by a user as well to start EPM with the last
+* saved ring, whenever he doubleclicks the .cmd file.
+*
+* Copyright (c) Netlabs EPM Distribution Project 2005
+*
+* $Id: epmlast.cmd,v 1.1 2005-11-23 22:31:04 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+
+env   = 'OS2ENVIRONMENT'
+Delay = 1  /* in s, time to wait before a new window is opened */
+
+call RxFuncAdd 'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs'
+call SysLoadFuncs
+
+parse arg args
+args = strip( args)
+rest = args
+if left( rest, 1) = '"' then
+   parse var rest '"'EpmExe'"' rest
+else
+   parse var rest EpmExe rest
+if left( rest, 1) = '"' then
+   parse var rest '"'rest'"'
+EpmArgs = rest
+
+if EpmExe = '' then
+do
+    /* Get BootDrive */
+   if \RxFuncQuery( 'SysBootDrive') then
+      BootDrive = SysBootDrive()
+   else
+      parse upper value value( 'PATH', , env) with ':\OS2\SYSTEM' -1 BootDrive +2
+   EpmExe = BootDrive'\os2\epm.exe'
+end
+
+if EpmArgs = '' then
+   EpmArgs = "'RestoreRing'"
+
+if args > '' then
+   call SysSleep Delay
+
+'start' EpmExe EpmArgs
+
+exit
+
