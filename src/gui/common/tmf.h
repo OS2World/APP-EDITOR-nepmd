3b545ab25ef975a9e361d5f983bc8d7566e4936f
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2006-10-07 19:53:41 +0000

    - Added missing header.

--- src/gui/common/tmf.h
+++ src/gui/common/tmf.h
@@ -1,8 +1,30 @@
+/****************************** Module Header *******************************
+*
+* Module Name: tmf.h
+*
+* Header for text message file functions
+*
+* Copyright (c) Netlabs EPM Distribution Project 2002
+*
+* $Id: tmf.h,v 1.2 2006-10-07 19:53:41 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
 
 #ifndef TMF_H
 #define TMF_H
 
-APIRET TmfGetMessage( PCHAR *pTable, ULONG cTable, PBYTE pbBuffer, ULONG cbBuffer, 
+APIRET TmfGetMessage( PCHAR *pTable, ULONG cTable, PBYTE pbBuffer, ULONG cbBuffer,
                       PSZ pszMessageName, PSZ pszFile, PULONG pcbMsg);
 #endif // TMF_H
 
