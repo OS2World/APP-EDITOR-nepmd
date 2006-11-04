d68ef20785052efdb39a76475ced610bbcd24fc0
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2006-11-04 16:13:24 +0000

    - Changed filename from SAVEOLD.CMD to REMEPM.CMD.

---
+++ trunk/src/netlabs/install/remepm.cmd
@@ -0,0 +1,279 @@
+/****************************** Module Header *******************************
+*
+* Module Name: remepm.cmd
+*
+* Syntax: remepm [NEPMD]
+*
+* Helper batch for to backup and delete an existing EPM of ?:\OS2\APPS
+*
+* Either a zip file (if ZIP.EXE found in PATH) or an xcopied tree is created
+* in NEPMD\backup. The name of the zip file is EPM_old.zip.
+*
+* After the backup (as a minimum: the binaries) proceeded successful, the
+* old files will be deleted.
+*
+* Copyright (c) Netlabs EPM Distribution Project 2002
+*
+* $Id: remepm.cmd,v 1.1 2006-11-04 16:13:24 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+/* Some header lines are used as help text */
+
+ /* initialize */
+ '@ECHO OFF';
+ call RxFuncAdd    'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs';
+ call SysLoadFuncs;
+ env = 'OS2ENVIRONMENT';
+ TRUE  = (1 = 1);
+ FALSE = (0 = 1);
+ Redirection = '>NUL 2>&1';
+
+ /* some OS/2 Error codes */
+ ERROR.NO_ERROR           =   0;
+ ERROR.INVALID_FUNCTION   =   1;
+ ERROR.FILE_NOT_FOUND     =   2;
+ ERROR.PATH_NOT_FOUND     =   3;
+ ERROR.ACCESS_DENIED      =   5;
+ ERROR.NOT_ENOUGH_MEMORY  =   8;
+ ERROR.INVALID_FORMAT     =  11;
+ ERROR.INVALID_DATA       =  13;
+ ERROR.NO_MORE_FILES      =  18;
+ ERROR.WRITE_FAULT        =  29;
+ ERROR.READ_FAULT         =  30;
+ ERROR.SHARING_VIOLATION  =  32;
+ ERROR.GEN_FAILURE        =  31;
+ ERROR.INVALID_PARAMETER  =  87;
+ ERROR.ENVVAR_NOT_FOUND   = 204;
+
+ /* defaults and further consts */
+ HelpStartLine  = 7
+ HelpEndLine    = 13
+ fDelete        = TRUE;
+ fDeleteList    = TRUE;
+ fRecoursive    = FALSE;  /* recoursive not required */
+ fUseZip        = TRUE;
+ rc             = ERROR.NO_ERROR;
+ ErrorQueueName = VALUE( 'NEPMD_RXQUEUE', , env);
+ ErrorMessage   = '';
+
+ /* make sure we are called on purpose */
+ ARG Parm .;
+ IF (Parm \= 'NEPMD') THEN
+ DO
+    SAY;
+    DO l = HelpStartLine TO HelpEndLine
+       SAY SUBSTR( SOURCELINE( l), 3);
+    END
+ END;
+ SAY;
+ SAY 'Do you want to continue? (Y/N)';
+ PULL Answer;
+ Answer = STRIP( Answer);
+ IF (ANSWER <> 'Y') THEN
+    EXIT( ERROR.GEN_FAILURE);
+
+ /* Get BootDrive */
+ IF \RxFuncQuery( 'SysBootDrive') THEN
+    BootDrive = SysBootDrive()
+ ELSE
+    PARSE UPPER VALUE VALUE( 'PATH', , env) WITH ':\OS2\SYSTEM' -1 BootDrive +2
+
+ /* get the base directory of the NEPMD installation */
+ PARSE Source . . CallName;
+ CallDir   = LEFT( CallName, LASTPOS( '\', CallName) - 1);
+ NepmdDir  = LEFT( CallDir,  LASTPOS( '\', CallDir) - 1);
+ BaseDir   = LEFT( NepmdDir, LASTPOS( '\', NepmdDir) - 1);
+ BackupDir = BaseDir'\backup';
+
+ GlobalVars = 'env TRUE FALSE Redirection BackupDir fUseZip fDelete fDeleteList fRecoursive';
+
+ DO UNTIL (TRUE)
+    /* check for old EPM */
+    EpmExe = BootDrive'\OS2\APPS\EPM.EXE'
+    IF (\FileExist( BootDrive'\OS2\APPS\EPM.EXE')) THEN
+    DO
+       rc = ERROR.FILE_NOT_FOUND;
+       ErrorMessage = 'Error: 'EpmExe' not found.'
+       LEAVE;
+    END;
+
+    /* get a list file */
+    ListFile = SysTempFilename( VALUE( 'TMP', , 'OS2ENVIRONMENT')'\nepmd.???');
+    IF (\fDeleteList) THEN
+       SAY 'Using ListFile "'ListFile'".';
+
+    /* get EPM.INI */
+    next = SysIni( 'USER', 'EPM', 'EPMIniPath');
+    IF next = 'ERROR:' THEN
+       EpmIni = BootDrive'OS2\EPM.INI';
+    ELSE
+       EpmIni = STRIP( next, 'T', '00'x);
+    /* remove attribs, except A */
+    rc = SysFileTree( EpmIni, 'EpmIni.', 'FO',,'*----');
+
+    /* check if ZIP.EXE is in PATH */
+    IF (fUseZip = TRUE) THEN
+    DO
+       next = SysSearchPath( 'PATH', 'ZIP.EXE');
+       fUseZip = (next > '');
+    END;
+
+    /* create backup path */
+    rest = BackupDir;
+    last = '';
+    i = 0;
+    /* strip \\machine\resource for UNC filename */
+    IF LEFT( BackupDir, 2) = '\\' THEN
+    DO
+       p1 = POS( '\', BackupDir, 3);
+       p2 = POS( '\', BackupDir, p1 + 1);
+       last = LEFT( BackupDir, MAX( p2 - 1, 0));  /* last: without trailing \ */
+       rest = SUBSTR( BackupDir, p2 + 1);         /* rest: without leading \ */
+    END;
+    /* strip drive for full filename */
+    IF SUBSTR( BackupDir, 2, 2) = ':\' THEN
+       PARSE VAR BackupDir last'\'rest;  /* last: without trailing \, rest: without leading \ */
+    /* create entire tree to ensure it exists */
+    DO WHILE rest <> ''
+       PARSE VAR rest next'\'rest;
+       last = last'\'next;
+       CALL SysMkDir( last);
+    END;
+
+    /* now backup and remove */
+    SAY 'Backing up old EPM files to directory' BackupDir;
+
+    IF EPMIni \= '' THEN
+       rc = BackupFiles( EPMIni,                         BackupDir'\OS2',          ListFile);
+    rc = BackupFiles( BootDrive'\OS2\APPS\EPM*',         BackupDir'\OS2\APPS',     ListFile);
+    IF (rc \= 0) THEN fDelete = 0;
+    rc = BackupFiles( BootDrive'\OS2\APPS\DLL\ETK*.DLL', BackupDir'\OS2\APPS\DLL', ListFile);
+    IF (rc \= 0) THEN fDelete = 0;
+    rc = BackupFiles( BootDrive'\OS2\APPS\DLL\EPM*.DLL', BackupDir'\OS2\APPS\DLL', ListFile);
+    rc = BackupFiles( BootDrive'\OS2\APPS\*.EX',         BackupDir'\OS2\APPS',     ListFile);
+    IF (rc \= 0) THEN fDelete = 0;
+    rc = BackupFiles( BootDrive'\OS2\APPS\*.E',          BackupDir'\OS2\APPS',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\APPS\*.ERX',        BackupDir'\OS2\APPS',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\APPS\ACTIONS.LST',  BackupDir'\OS2\APPS',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\APPS\*.BMP',        BackupDir'\OS2\APPS',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\HELP\EPM*.HLP',     BackupDir'\OS2\HELP',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\HELP\ETK*.HLP',     BackupDir'\OS2\HELP',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\HELP\TREE.HLP',     BackupDir'\OS2\HELP',     ListFile);
+    rc = BackupFiles( BootDrive'\OS2\HELP\REFLOW.HLP',   BackupDir'\OS2\HELP',     ListFile);
+    /* Note: \OS2\EPM* would process files in \OS2\ARCHIVES */
+
+    /* now delete files, ignore errors here */
+    IF (fDelete = TRUE) THEN
+       SAY 'Removing old EPM files';
+    ELSE
+       SAY 'Test mode: listing old EPM files';
+
+    DO WHILE (LINES( ListFile))
+       ThisFile = STRIP( LINEIN( ListFile));
+       IF (ThisFile = '') THEN ITERATE;
+
+       /* don't remove EPM.INI */
+       IF TRANSLATE( filespec( 'N', THISFILE)) = 'EPM.INI' THEN ITERATE;
+
+       rcx = SysFileTree( ThisFile, 'File.', 'F',,'-----');
+
+       SAY ThisFile;
+       IF (fDelete = TRUE) THEN
+          rcx = SysFileDelete( ThisFile);
+
+       rc = ERROR.NO_ERROR;
+    END;
+
+    CALL STREAM ListFile, 'C', 'CLOSE';
+    IF (fDeleteList = TRUE) THEN
+       rcx = SysFileDelete( ListFile);
+
+ END;
+
+ /* report error message */
+ SELECT
+    /* no error here */
+    WHEN (rc = 0) THEN NOP;
+
+    /* called by frame program: insert error */
+    /* message into standard REXX queue     */
+    WHEN (ErrorQueueName \= '') THEN
+    DO
+       rcx = RXQUEUE( 'SET', ErrorQueueName);
+       PUSH ErrorMessage;
+    END;
+
+    /* called directly, method */
+    OTHERWISE
+    DO
+       SAY ErrorMessage;
+       'PAUSE';
+    END;
+ END;
+
+ EXIT( rc);
+
+/* ------------------------------------------------------------------------- */
+FileExist: PROCEDURE
+ PARSE ARG FileName;
+
+ RETURN(STREAM( Filename, 'C', 'QUERY EXISTS') > '');
+
+/* ------------------------------------------------------------------------- */
+BackupFiles: PROCEDURE EXPOSE (GlobalVars)
+ PARSE ARG FileName, DestDir, ListFile;
+
+ IF fRecoursive THEN      /* recoursive not required */
+ DO
+   DirOptions   = '/S /F /A-D';
+   XcopyOptions = '/H/O/T/S/R';
+   ZipOptions   = '-rSD';
+ END
+ ELSE
+ DO
+   DirOptions   = '/F /A-D';
+   XcopyOptions = '/H/O/T/R';
+   ZipOptions   = '-SD';
+ END;
+
+ DO UNTIL (TRUE)
+
+    /* check if filespec exists to avoid xcopy beeps */
+    IF \FileExist( FileName) THEN
+       LEAVE;
+
+    /* append filenames to listfile */
+    'DIR' FileName DirOptions '>>' ListFile '2>NUL';
+
+    /* copy the files */
+    IF fUseZip = 0 THEN
+       'XCOPY' FileName DestDir'\' XcopyOptions Redirection;
+    ELSE
+    DO
+       SavedDir = DIRECTORY();
+       CALL DIRECTORY BackUpDir;
+       'ZIP' ZipOptions BackupDir'\EPM_old' FileName Redirection;
+       CALL DIRECTORY SavedDir;
+    END;
+
+ END;
+
+ RETURN( rc);
+
+/* ------------------------------------------------------------------------- */
+HALT:
+ SAY;
+ SAY 'Interrupted by user.';
+ EXIT( ERROR.GEN_FAILURE);
+
