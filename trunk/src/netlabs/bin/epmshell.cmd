c2d33731d2042e907d4729523ad1a64c7da5cd7f
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2006-12-17 18:06:12 +0000

    First revision

---
+++ trunk/src/netlabs/bin/epmshell.cmd
@@ -0,0 +1,51 @@
+/****************************** Module Header *******************************
+*
+* Module Name: epmshell.cmd
+*
+* Syntax: epmshell [directory]
+*
+* Helper batch for to open a new EPM window with an EPM command shell
+* window. The path of the shell is changed to the submitted directory, if
+* any.
+*
+* To use this as an extension for XWorkplace's "Configuration folder" ->
+* "Command prompts" folder, place a reference object (or a copy) of the
+* "EPM Shell" program object in the "Command prompts" folder.
+*
+*    EXENAME     <path>epmshell.cmd
+*    PARAMETERS  (none)
+*    STARTUPDIR  (none)
+*
+* Also activate MINIMIZED=YES for it.
+*
+* This script is required because the parameter resolution of XWP for %*
+* is buggy: A WPS program object with the parameters '%*' (including the
+* quotes) is not recognized as a % parameter. XWP would append the directory
+* instead of inserting it where the %* stands. Otherwise a program object
+* with the following parameters would work:
+*
+*    EXENAME     epm.exe
+*    PARAMETERS  'shell cdd {%*}'
+*    STARTUPDIR  (none)
+*
+* Copyright (c) Netlabs EPM Distribution Project 2006
+*
+* $Id: epmshell.cmd,v 1.1 2006-12-17 18:06:12 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+
+parse arg args
+"@start epm 'shell cdd {"args"}'"
+exit
+
