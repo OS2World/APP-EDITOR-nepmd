f14af097d89dfc9f520c2764b09545350af8f510
jbs <jbs@72c63837-6175-e011-9ffd-00145e329d72>
2007-03-02 04:16:19 +0000

    - This is a replacement for the previously incomplete program. It will   set or reset NEPMD as the default program for the types and filters   with which it is associated. - The program works in unattended mode when a parameter of 'SET' or 'RESET'   is used. - Without a parameter the program works in interactive mode.

--- trunk/src/netlabs/bin/epmdefassocs.cmd
+++ trunk/src/netlabs/bin/epmdefassocs.cmd
@@ -4,7 +4,7 @@
 *
 * Copyright (c) Netlabs EPM Distribution Project 2002
 *
-* $Id: epmdefassocs.cmd,v 1.1 2006-12-20 20:44:00 aschn Exp $
+* $Id: epmdefassocs.cmd,v 1.2 2007-03-02 04:16:19 jbs Exp $
 *
 * ===========================================================================
 *
@@ -18,274 +18,263 @@
 * General Public License for more details.
 *
 ****************************************************************************/
+call RxFuncAdd 'SysLoadFuncs', 'REXXUTIL', 'SysLoadFuncs'
+call SysLoadFuncs
+call RxFuncAdd 'WPToolsLoadFuncs', 'WPTOOLS', 'WPToolsLoadFuncs'
+call WPToolsLoadFuncs
 
-/* Not ready, actually lists only settings, but doesn't change anything */
-/* It can be run without risk!                                          */
+signal on novalue                                  /* for debug purposes */
 
+GlobalVars = 'Objs. Assocs. AssocTypes action ERROR. env TRUE FALSE Redirection interactive action defaultoption';
+
+call Init                                          /* Initialize variables */
+
+tracevar =  value('DEFASSOCTRACE',, env)           /* for debug purposes */
+if tracevar == "" then
+   tracevar = 'n'
+trace value tracevar
+
+parse arg args                                     /* Process parameters, if any */
+call ProcessArgs args
+
+call GetAssocs                                     /* Get association for selected objects */
+
+do o = 1 to Objs.0
+   ObjHandle = GetObjHandle(Objs.o.ID)
+Relist:
+   option = defaultoption
+   i = 0
+   ActionList = ''
+   outline = ''
+   do t = 1 to words(AssocTypes)
+      AssocType = word(AssocTypes, t)
+      do a = 1 to Objs.o.Assocs.AssocType.0
+         if words(Objs.o.Assocs.AssocType.a.AssocObjs) > 0 then
+            if (action == 'SET' & word(Objs.o.Assocs.AssocType.a.AssocObjs, 1) \= ObjHandle) | ,
+               (action == 'RESET' & word(Objs.o.Assocs.AssocType.a.AssocObjs, 1) == ObjHandle) then
+               do
+                  ActionList = ActionList AssocType a
+                  if interactive = TRUE then
+                     do
+                        if i == 0 then
+                           call ShowHeading Objs.o.Title, Objs.o.type.list, Objs.o.filter.list
+                        if i // Objs.o.Assocs.Number_per_line == 0 then
+                           do
+                              say outline
+                              outline = ''
+                           end
+                        i = i + 1
+                        outline = outline || right(i, 2) || ') ' || left(Objs.o.Assocs.AssocType.a, Objs.o.Assocs.Maxlen)
+                     end
+               end
+      end
+   end
+
+   if interactive == TRUE & words(ActionList) > 0 then
+      do
+         if length(outline) \= 0 then
+            say outline
+         say
+         say 'To remove a type or filter from the list, enter its number.'
+         if action == 'SET' then
+            say 'To make 'Objs.o.Title' the default for all these types and filters, type ''Y''.'
+         else
+            say 'To remove 'Objs.o.Title' as the default for all these types and filters, type ''Y''.'
+         if action == 'SET' then
+            say 'Type ''N'' to reject making 'Objs.o.Title' the default application.'
+         else
+            say 'Type ''N'' to keep 'Objs.o.Title' as the default application.'
+         say
+         option = translate(strip(linein()))
+         if datatype(option) == 'NUM' then
+            if option <= i then
+               do
+                  t = word(ActionList, 2 * option - 1)
+                  a = word(ActionList, 2 * option)
+                  Objs.o.Assocs.t.a.AssocObjs = delword(Objs.o.Assocs.t.a.AssocObjs, wordpos(ObjHandle, Objs.o.Assocs.t.a.AssocObjs), 1)
+                  if action == 'SET' then
+                     Objs.o.Assocs.t.a.AssocObjs = ObjHandle Objs.o.Assocs.t.a.AssocObjs
+                  else
+                     Objs.o.Assocs.t.a.AssocObjs = Objs.o.Assocs.t.a.AssocObjs ObjHandle
+                  signal Relist
+               end
+            else
+               nop
+         else
+            if wordpos(option, 'Y N') == 0 then
+               option = defaultoption
+      end
 /*
-
-The selections may be submitted to this script as:
-   "/I:<NEPMD_ /E:epm.exe /D /A"
-
-UI ideas:
-
-------------------------------------------------------------------------------
-Change object associations (1/4)
-(Skip this section if I, E or T params were submitted, then just show
-the filter values)
-
-Select program object filters:
-
-   I  Object id filter : <NEPMD_
-   E  Executable filter: epm.exe
-   T  Title filter     :
-
-   An abbreviation matches, e.g. "<NEPMD_" for the object id or "epm"
-   for the executable. You can use multiple filters simultaneously,
-   where filters were AND-combined.
-
-   Press I, E or T and <Return> to change a filter's value,
-   press <Return> to accept the current filters or
-   press C <Return> to cancel:
-
-------------------------------------------------------------------------------
-Change object associations (2/4)
-
-With the given filters, the following program objects were found.
-
-Select program objects to process:
-
-      Object id       Executable    Title
-   1  <NEPMD_EPM>
-   2  ...
-
-   Type a space- or comma-separated list of numbers, (e.g. 1,3)
-   and press <Return> to specify some of the listed objects,
-   press <Return> to accept all listed objects,
-   press B <Return> to go back to the start of the selection or
-   press C <Return> to cancel:
-
-Following objects were selected:
-   <NEPMD_EPM>         epm.exe        EPM
-
-------------------------------------------------------------------------------
-Change object associations (3/4)
-(Skip this section if R, D, L or W params were submitted, then just show
-the filter values)
-
-Select an action for these objects:
-
-   R  remove the object's associations
-   D  make the object the default associated object
-   L  make the object the last associated object
-   Y  specify new .FILETYPE associations for the object
-   N  specify new filename associations for the object
-   W  open WPS properties of the object
-
-   Type R, D, L or W and <Return> to select an action,
-   press B <Return> to go back to the start of the selection or
-   press C <Return> to cancel:
-
-------------------------------------------------------------------------------
-Change object associations (4/4)
-(Skip this section if P or A params were submitted, then just show
-the filter values)
-
-Select how to procede:
-
-   P  pause before changing every association
-   A  process all objects and associations without pause
-
-   Type P or A and <Return> to start the action,
-   press B <Return> to go back to the start of the selection or
-   press C <Return> to cancel:
-
+   else
+      say '"'Objs.o.Title'" is already set to be the default application for all types and filters.'
 */
 
+   if option == 'Y' then
+      do t = 1 to words(AssocTypes)
+         AssocType = word(AssocTypes, t)
+         do a = 1 to Objs.o.Assocs.AssocType.0
+            wp = wordpos(ObjHandle, Objs.o.Assocs.AssocType.a.AssocObjs)
+            if action == 'SET' then
+               do
+                  if wp == 0 then
+                     Objs.o.Assocs.AssocType.a.AssocObjs = ObjHandle Objs.o.Assocs.AssocType.a.AssocObjs
+                  else
+                     Objs.o.Assocs.AssocType.a.AssocObjs = ObjHandle delword(Objs.o.Assocs.AssocType.a.AssocObjs, wp, 1)
+                  rcx = SysIni(, 'PMWP_ASSOC_' || AssocType, Objs.o.Assocs.AssocType.a, translate(Objs.o.Assocs.AssocType.a.AssocObjs, '00'x, ' '))
+                  if rcx \= '' then
+                     say 'Set default error for 'Objs.o.Assocs.AssocType.a': 'rcx
+               end
+            else
+               do
+                  /* Reset only if this is the default and there is more than one association */
+                  if (wp == 1 & words(Objs.o.Assocs.AssocType.a.AssocObjs) > 1) then
+                        Objs.o.Assocs.AssocType.a.AssocObjs = substr(Objs.o.Assocs.AssocType.a.AssocObjs, wordindex(Objs.o.Assocs.AssocType.a.AssocObjs, 2)) || ObjHandle
+                        rcx = SysIni(, 'PMWP_ASSOC_' || AssocType, Objs.o.Assocs.AssocType.a, translate(Objs.o.Assocs.AssocType.a.AssocObjs, '00'x, ' '))
+                        if rcx \= '' then
+                           say 'Reset default error for 'Objs.o.Assocs.AssocType.a': 'rcx
+                     end
+               end
 
-/* REXX */
+         end
+      end
+end
 
-INIT:
- SIGNAL ON HALT NAME HALT
+return
 
- '@ECHO OFF';
- env   = 'OS2ENVIRONMENT';
- TRUE  = (1 = 1);
- FALSE = (0 = 1);
- CrLf  = '0d0a'x;
- Redirection = '>NUL 2>&1';
- GlobalVars = 'env TRUE FALSE Redirection ERROR';
+ShowHeading: procedure expose (GlobalVars)
+   parse arg ObjTitle, Typelist, FilterList
+   call SysCls
+   say
+   say '"'ObjTitle'" is associated with the following types:'
+   say
+   say TypeList
+   say
+   say 'and with the following filters:'
+   say
+   say FilterList
+   say
+   say
+   call charout , '"'ObjTitle'" is '
+   if action == 'SET' then
+      call charout , 'not '
+   say 'currently set as the default application for:'
+return
 
- /* some OS/2 Error codes */
- ERROR.NO_ERROR           =   0;
- ERROR.INVALID_FUNCTION   =   1;
- ERROR.FILE_NOT_FOUND     =   2;
- ERROR.PATH_NOT_FOUND     =   3;
- ERROR.ACCESS_DENIED      =   5;
- ERROR.NOT_ENOUGH_MEMORY  =   8;
- ERROR.INVALID_FORMAT     =  11;
- ERROR.INVALID_DATA       =  13;
- ERROR.NO_MORE_FILES      =  18;
- ERROR.WRITE_FAULT        =  29;
- ERROR.READ_FAULT         =  30;
- ERROR.SHARING_VIOLATION  =  32;
- ERROR.GEN_FAILURE        =  31;
- ERROR.INVALID_PARAMETER  =  87;
- ERROR.ENVVAR_NOT_FOUND   = 204;
+Init: procedure expose (GlobalVars)
+   '@ECHO OFF';
+   env   = 'OS2ENVIRONMENT';
+   TRUE  = (1 = 1);
+   FALSE = (0 = 1);
+   CrLf  = '0d0a'x;
+   Redirection = '>NUL 2>&1';
 
- rc = ERROR.NO_ERROR;
+   /* some OS/2 Error codes */
+   ERROR.NO_ERROR           =   0;
+   ERROR.INVALID_FUNCTION   =   1;
+   ERROR.FILE_NOT_FOUND     =   2;
+   ERROR.PATH_NOT_FOUND     =   3;
+   ERROR.ACCESS_DENIED      =   5;
+   ERROR.NOT_ENOUGH_MEMORY  =   8;
+   ERROR.INVALID_FORMAT     =  11;
+   ERROR.INVALID_DATA       =  13;
+   ERROR.NO_MORE_FILES      =  18;
+   ERROR.WRITE_FAULT        =  29;
+   ERROR.READ_FAULT         =  30;
+   ERROR.SHARING_VIOLATION  =  32;
+   ERROR.GEN_FAILURE        =  31;
+   ERROR.INVALID_PARAMETER  =  87;
+   ERROR.ENVVAR_NOT_FOUND   = 204;
 
- /* initialize */
- CALL RxFuncAdd 'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs';
- CALL SysLoadFuncs;
+   rc = ERROR.NO_ERROR;
 
- CALL RxFuncAdd 'WPToolsLoadFuncs', 'WPTOOLS', 'WPToolsLoadFuncs';
- CALL WPToolsLoadFuncs;
+   Objs.      = ''
+   AssocTypes = 'TYPE FILTER'
+return
 
+ProcessArgs: procedure expose (GlobalVars)
+   /* To do: Accept a list of objects as parameters */
+   parse upper arg action .                           /* Process parameters, if any */
+   /* Hard-coded object id's for now. */
+   Objs.1.ID = '<NEPMD_EPM>'
+   Objs.2.ID = '<NEPMD_EPM_E>'
+   Objs.3.ID = '<NEPMD_EPM_EDIT_MACROFILE>'
+   Objs.4.ID = '<NEPMD_EPM_ERX>'
+   Objs.5.ID = '<NEPMD_EPM_TEX>'
+   Objs.6.ID = '<NEPMD_EPM_BIN>'
+   Objs.0    = 6
+   action = strip(action)
+   interactive = TRUE
+   action = 'SET'                                     /* Set default values */
+   defaultoption = 'N'
+   if action \= '' then
+      if wordpos(action, 'SET RESET') > 0 then
+         do
+            interactive = FALSE
+            defaultoption = 'Y'
+         end
+return
 
- /* query all abstract objects */
- rcx = SysIni( 'USER', 'PM_Abstract:Objects', 'All:', 'Keyw.')
- IF (rcx = 'ERROR:') THEN
-    EXIT( ERROR.PATH_NOT_FOUND);
+GetAssocs: procedure expose (GlobalVars)
+   do o = 1 to Objs.0
+      Objs.o.Assocs. = ''
+      Objs.o.Assocs.type.0 = 0
+      Objs.o.Assocs.filter.0 = 0
+      ObjSetup  = GetObjSetup(Objs.o.ID)
+      parse var ObjSetup . 'ASSOCTYPE=' Objs.o.type.list ',,;' .
+      parse var ObjSetup . 'ASSOCFILTER=' Objs.o.filter.list ',,;' .
+      parse var ObjSetup . 'TITLE=' Objs.o.Title ';' .
+      Objs.o.Assocs.Maxlen = 0
+      i = 0
+      t = strip(Objs.o.type.list, 'T', ',')
+      do while t \= ''
+         i = i + 1
+         parse var t Objs.o.Assocs.type.i ',' t
+         l = length(Objs.o.Assocs.type.i)
+         if l > Objs.o.Assocs.Maxlen then
+            Objs.o.Assocs.Maxlen = l
+      end
+      Objs.o.Assocs.type.0 = i
+      i = 0
+      t = strip(Objs.o.filter.list, 'T', ',')
+      do while t \= ''
+         i = i + 1
+         parse var t Objs.o.Assocs.filter.i ',' t
+         l = length(Objs.o.Assocs.filter.i)
+         if l > Objs.o.Assocs.Maxlen then
+            Objs.o.Assocs.Maxlen = l
+      end
+      Objs.o.Assocs.filter.0 = i
+      Objs.o.Assocs.Maxlen = Objs.o.Assocs.Maxlen + 2
 
- SAY 'Found 'Keyw.0' abstract objects'
+      parse value SysTextScreenSize() with . cols
+      if cols == 0 then
+         cols = 80
+      Objs.o.Assocs.Number_per_line = trunc(cols / (Objs.o.Assocs.Maxlen + 4))
 
- ObjectIdFilter = '<NEPMD_'
- ExeNameFilter  = 'epm.exe'
- TitleFilter    = ''
- Action = 'D'
+      do t = 1 to words(AssocTypes)
+         AssocType = word(AssocTypes, t)
+         App = 'PMWP_ASSOC_' || AssocType
+         do a = 1 to Objs.o.Assocs.AssocType.0
+            Objs.o.Assocs.AssocType.a.AssocObjs = translate(SysIni(, App, Objs.o.Assocs.AssocType.a), ' ', '00'x)
+         end
+      end
+   end
+return
 
-RESTART:
- DO k = 1 TO Keyw.0
-    Keyw = Keyw.k;
-    Entry = SysIni( 'USER', 'PM_Abstract:Objects', Keyw);
-    IF (Entry = 'ERROR:') THEN
-       ITERATE;
+GetObjHandle: procedure expose (GlobalVars)
+   parse arg objid
+   BinObjHandle = reverse(SysIni(, 'PM_Workplace:Location', objid))
+   DecObjHandle = c2d(BinObjHandle)
+return DecObjHandle
 
-    /* get class from entry */
-    PARSE VAR Entry +4 Class'00'x rest;
+GetObjSetup: procedure expose (GlobalVars)
+   parse arg obj
+   call WPToolsQueryObject obj,,, 'setup'
+   if right(setup, 1) \= ';' then
+      setup = setup || ';'
+return setup
 
-    /* get hex handle from Keyw */
-    IF (Class = 'WPProgram') THEN
-       hObjHex = 2''RIGHT( Keyw, 4, 0);
-    ELSE
-       hObjHex = 3''RIGHT( Keyw, 4, 0);
-
-    /* convert to decimal as well for later use */
-    hObjDec = X2D( hObjHex)
-
-    /* process program objects only */
-    IF (Class <> 'WPProgram') THEN
-       ITERATE;
-/*
-Hex: [2B1F2]  Dec: [176626]  Class = [WPProgram]
-                             Title = [EPM]
-*/
-
-    /* query that object handle */
-    Obj = '#'hObjHex;
-    /* drop required, otherwise wrong objects were listed */
-    DROP Class;
-    DROP Title;
-    DROP Setup;
-    DROP Location;
-    rcx = WpToolsQueryObject( Obj, Class, Title, Setup, Location);
-    IF (rcx <> 1) THEN
-       ITERATE;
-
-    PARSE VAR Setup 'OBJECTID='ObjectId';'rest;
-    PARSE VAR Setup 'EXENAME='ExeName';'rest;
-
-    /* remove path from ExeName */
-    ExeName = FILESPEC( 'n', ExeName);
-
-    /* Check if object matches the filters */
-    fMatch = FALSE;
-    DO 1
-       IF (ObjectIdFilter <> '' & ,
-           POS( ObjectIdFilter, ObjectId) <> 1) THEN
-          LEAVE;
-       IF (ExeNameFilter <> '' & ,
-           POS( TRANSLATE( ExeNameFilter), TRANSLATE( ExeName)) <> 1) THEN
-          LEAVE;
-       IF (TitleFilter <> '' & ,
-           POS( TRANSLATE( TitleFilter), TRANSLATE( Title)) <> 1) THEN
-          LEAVE;
-       fMatch = TRUE;
-    END;
-    IF (fMatch = FALSE) THEN
-       ITERATE;
-
-    SAY ObjectId' - 'ExeName' - 'Title;
-/**/
-    CALL CHAROUT, '   Press <Return> to continue, B <Return> to restart or C <Return> to cancel: ';
-    Answer = STRIP( LINEIN());
-    IF TRANSLATE( Answer) = 'C' THEN SIGNAL HALT;
-    IF TRANSLATE( Answer) = 'B' THEN SIGNAL RESTART;
-    /* PAUSE or SysGetKey doesn't work in an EPM Shell: */
-    /* A <RETURN> is processed doubled after PAUSE and  */
-    /* SysGetKey won't wait for a key.                  */
-/**/
-
-    IF (Action = 'W') THEN
-    DO
-       rcx = WpToolsSetObjectData( Obj, 'OPEN=SETTINGS;');
-       ITERATE;
-    END;
-
-    IF (WORDPOS( Action, 'R D L') = 0) THEN
-       ITERATE;
-
-    PARSE VAR Setup 'ASSOCFILTER='List.1';'rest
-    PARSE VAR Setup 'ASSOCTYPE='List.2';'rest
-    Appl.1 = 'PMWP_ASSOC_FILTER'
-    Appl.2 = 'PMWP_ASSOC_TYPE'
-
-    /* query for all association filters the zero-separated hObhDec list */
-    DO a = 1 TO 2
-       rest = List.a;
-       DO WHILE (LENGTH( rest) > 0)
-          PARSE VAR rest next','rest;
-          hObjList = SysIni( 'USER', Appl.a, next);
-          IF hObjList = 'ERROR:' THEN
-             ITERATE;
-          /* translate to space-separated list for easier processing */
-          OldList = TRANSLATE( hObjList, ' ', '00'x)
-          /* search handle for current program object */
-          wp = WORDPOS( hObjDec, OldList)
-say 'OldList = 'next': 'OldList', wp = 'wp', hObjDec = 'hObjDec', Object id = 'ObjectId
-          IF (wp > 0) THEN
-          DO
-             NewList = DELWORD( OldList, wp, 1);
-             IF (Action = 'D') THEN
-                NewList = hObjDec NewList;
-             ELSE IF (Action = 'L') THEN
-                NewList = NewList hObjDec;
-             /* reformat handle list */
-             NewList = SPACE( NewList, 1, '00'x)'00'x
-say 'NewList = 'next': 'TRANSLATE( NewList, '.', '00'x)
-/*
-**** Better don't write anything to OS2.INI as long as the UI isn't ready ****
-             rcx = SysIni( 'USER', Appl.a, next, NewList);
-             IF rcx = 'ERROR:' THEN
-             DO
-                SAY 'Error writing ini key 'Appl.a' -> 'next'.';
-                SAY 'Old value was:' TRANSLATE( hObjList, '.', '00'x);
-                EXIT( ERROR.WRITE_FAULT);
-             END;
-*/
-          END;
-       END;
-    END;
-
- END
-
- EXIT( rc);
-
-/* ------------------------------------------------------------------------- */
-
-HALT:
- SAY;
- SAY 'Interrupted by user.';
- EXIT( ERROR.GEN_FAILURE);
-
+novalue:
+   say 'Unset value on line 'sigl
+   say 'Text: 'sourceline(sigl)
+   exit
