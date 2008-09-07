891af6bf4a13ca6217831462500755e6bee95015
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2008-09-07 23:15:14 +0000

    First revision

---
+++ src/netlabs/install/recrobj.cmd
@@ -0,0 +1,274 @@
+/****************************** Module Header *******************************
+*
+* Module Name: recrobj.cmd
+*
+* Syntax: recrobj [FORCE]
+*
+* If FORCE is not specified, the user will be asked before further execution.
+*
+* This CMD is able to recreate objects and ini entries, if the export file
+* was created before. Even when the recommended way to recreate them is to
+* repeat the WarpIN installation, this CMD could be useful if the WPI file
+* is not available, but the files are present.
+*
+* Note: The WarpIN database entries will not be recreated.
+*
+* Copyright (c) Netlabs EPM Distribution Project 2008
+*
+* $Id: recrobj.cmd,v 1.1 2008-09-07 23:14:19 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+
+'@ECHO OFF'
+
+/* ----------------- Standard CMD initialization follows ----------------- */
+SIGNAL ON HALT NAME Halt
+SIGNAL ON ERROR NAME Error
+SIGNAL ON SYNTAX NAME Error
+
+env   = 'OS2ENVIRONMENT'
+TRUE  = (1 = 1)
+FALSE = (0 = 1)
+CrLf  = '0d0a'x
+Redirection = '>NUL 2>&1'
+PARSE SOURCE . . ThisFile
+GlobalVars = 'env TRUE FALSE Redirection ERROR. ThisFile'
+
+/* some OS/2 Error codes */
+ERROR.NO_ERROR           =   0
+ERROR.INVALID_FUNCTION   =   1
+ERROR.FILE_NOT_FOUND     =   2
+ERROR.PATH_NOT_FOUND     =   3
+ERROR.ACCESS_DENIED      =   5
+ERROR.NOT_ENOUGH_MEMORY  =   8
+ERROR.INVALID_FORMAT     =  11
+ERROR.INVALID_DATA       =  13
+ERROR.NO_MORE_FILES      =  18
+ERROR.WRITE_FAULT        =  29
+ERROR.READ_FAULT         =  30
+ERROR.SHARING_VIOLATION  =  32
+ERROR.GEN_FAILURE        =  31
+ERROR.INVALID_PARAMETER  =  87
+ERROR.ENVVAR_NOT_FOUND   = 204
+
+rc = ERROR.NO_ERROR
+
+CALL RxFuncAdd 'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs'
+CALL SysLoadFuncs
+/* ----------------- Standard CMD initialization ends -------------------- */
+
+/* ------------- Configuration ---------------- */
+/* ExportFilename will be searched in the same dir as this file. Better */
+/* use the netlabs tree to find that file easily, even when the user    */
+/* tree was created elsewhere out of the NEPMD rootdir, e.g. in %HOME%. */
+ExportFilename       = 'recrobj.dat'
+/* -------------------------------------------- */
+
+ARG Args
+Args = STRIP( Args)
+
+IF (Args <> 'FORCE') THEN
+DO
+   /* Skip header */
+   rcx = STREAM( ThisFile, 'c', 'open read')
+   DO i = 1 TO 3
+      rcx = LINEIN( ThisFile)
+   END
+   /* Show help */
+   DO WHILE (RIGHT( ThisLine, 2) \= '*/')
+      i = i + 1
+      ThisLine = LINEIN( Thisfile)
+      /* Show only these line numbers */
+      /* Omit syntax help */
+      IF i < 9 THEN ITERATE
+      IF i > 15 THEN LEAVE
+      SAY SUBSTR( ThisLine, 3)
+   END
+   rcx = STREAM( ThisFile, 'c', 'close')
+
+   /* Ask */
+   SAY
+   SAY 'Do you want to proceed? (Type Y or any other key to abort):'
+   Answer = LINEIN()
+   Answer = TRANSLATE( STRIP( Answer))
+   IF Answer <> 'Y' THEN
+      SIGNAL Halt
+END
+
+/* Read ExportFile */
+lp = LASTPOS( '\', ThisFile)
+ExportFile = SUBSTR( ThisFile, 1, lp)ExportFilename
+IF (STREAM( ExportFile, 'c', 'query exists') = '') THEN
+DO
+   SAY 'Error: ExportFile "'ExportFile'" doesn''t exist.'
+   EXIT( ERROR.FILE_NOT_FOUND)
+END
+ExportLine.  = ''
+ExportLine.0 = 0
+l = 0
+rcx = STREAM( ExportFile, 'c', 'open read')
+DO WHILE LINES( ExportFile) > 0
+   l = l + 1
+   ExportLine.l = LINEIN( ExportFile)
+END
+ExportLine.0 = l
+rcx = STREAM( ExportFile, 'c', 'close')
+
+/* Get BootDrive */
+IF \RxFuncQuery( 'SysBootDrive') THEN
+   BootDrive = SysBootDrive()
+ELSE
+   PARSE UPPER VALUE VALUE( 'PATH',, env) WITH ':\OS2\SYSTEM' -1 BootDrive +2
+
+/* Get TargetPath. Supports one common TargetPath for all packages only. */
+TargetPath = ''
+DO l = 1 TO ExportLine.0
+   PARSE VAR ExportLine.l num 'TARGETPATH='next
+   IF (next <> '') THEN
+   DO
+      TargetPath = next
+      LEAVE
+   END
+END
+IF (TargetPath = '') THEN
+DO
+   SAY 'Error: "TARGETPATH=" not found in "'ExportFile'".'
+   EXIT( ERROR.INVALID_DATA)
+END
+
+/* Replace ?:\ and $(1) in ExportLines */
+DO l = 1 TO ExportLine.0
+   ThisLine = ExportLine.l
+
+   OldString = '?:\'
+   NewString = BootDrive'\'
+   ThisLine = ReplaceString( OldString, ThisLine, NewString)
+
+   OldString = '$(1)'
+   NewString = TargetPath
+   ThisLine = ReplaceString( OldString, ThisLine, NewString)
+
+   IF ExportLine.l <> ThisLine THEN
+   DO
+      /*
+      SAY 'Old: 'ExportLine.l
+      SAY 'New: 'ThisLine
+      */
+      ExportLine.l = ThisLine
+   END
+
+END
+
+/* Add ini entries */
+DO l = 1 TO ExportLine.0
+   PARSE VAR ExportLine.l num 'PROFILE='next
+   IF (next <> '') THEN
+   DO
+      /* With the syntax how WarpIN saves the line, */
+      /* a filename can't be specified as Ini:      */
+      PARSE VAR next Ini'\'Appl'\'Key'|'Val
+      /*
+      SAY "next = SysIni( "Ini", "Appl", "Key", "Val"'00'x)"
+      */
+      next = SysIni( Ini, Appl, Key, Val'00'x)
+      IF (next = 'ERROR:') THEN
+      DO
+         SAY 'Error: "'Key'" not written to ini "'Ini'", appl = "'Appl'", val = "'Val'.'
+         EXIT( ERROR.WRITE_FAULT)
+      END
+   END
+END
+
+/* Create objects */
+DO l = 1 TO ExportLine.0
+   PARSE VAR ExportLine.l num 'OBJECT='next
+   IF (next <> '') THEN
+   DO
+      PARSE VAR next Class'|'Title'|'Dest'|'Setup
+      UpdateReplaceFail = 'U'
+      /*
+      SAY "rcx = SysCreateObject( "Class", "Title", "Dest", "Setup", "UpdateReplaceFail")"
+      */
+      rcx = SysCreateObject( Class, Title, Dest, Setup, UpdateReplaceFail)
+      IF (rcx <> 1) THEN
+      DO
+         IF Class = 'WPShadow' THEN
+            SAY 'Error: Shadow object with setup "'Setup'" not created.'
+         ELSE
+            SAY 'Error: Object "'Title'" not created.'
+         EXIT( ERROR.WRITE_FAULT)
+      END
+   END
+END
+
+/* Execute postinstall calls */
+DO l = 1 TO ExportLine.0
+   PARSE VAR ExportLine.l num 'EXECUTE='next
+   IF (next <> '') THEN
+   DO
+      SearchString = 'nlsetup.exe NEPMD'
+      IF (TRANSLATE( RIGHT( next, LENGTH( SearchString))) = TRANSLATE( SearchString)) THEN
+      DO 1
+         /* Don't call nlsetup here! That would also try to recreate the */
+         /* export file and also rename user dirs.                       */
+         /* Currently hardcoded, keep in sync with NLSETUP.CMD/.EXE:     */
+         /*
+         SAY 'CALL' TargetPath'\netlabs\install\usertree.cmd'; IF (rc \= 0) THEN LEAVE;
+         SAY 'CALL' TargetPath'\netlabs\install\special.cmd';  IF (rc \= 0) THEN LEAVE;
+         SAY 'CALL' TargetPath'\netlabs\install\dyncfg.cmd';   IF (rc \= 0) THEN LEAVE;
+         */
+         'CALL' TargetPath'\netlabs\install\usertree.cmd'; IF (rc \= 0) THEN LEAVE;
+         'CALL' TargetPath'\netlabs\install\special.cmd';  IF (rc \= 0) THEN LEAVE;
+         'CALL' TargetPath'\netlabs\install\dyncfg.cmd';   IF (rc \= 0) THEN LEAVE;
+      END
+      ELSE
+         /*
+         SAY 'CALL' next
+         */
+         'CALL' next
+   END
+END
+
+EXIT( rc)
+
+/* ----------------------------------------------------------------------- */
+/* Like CHANGESTR */
+ReplaceString: PROCEDURE EXPOSE (GlobalVars)
+   PARSE ARG OldString, SourceString, NewString
+
+   Startp = 1
+   DO FOREVER
+      p1 = POS( OldString, SourceString, Startp)
+      IF (p1 = 0) THEN
+         LEAVE
+      SourceString = INSERT( NewString,,
+                             DELSTR( SourceString, p1, LENGTH( OldString)),,
+                             p1 - 1)
+      Startp = p1 + LENGTH( NewString)
+   END
+
+   RETURN( SourceString)
+
+/* ----------------------------------------------------------------------- */
+Halt:
+   SAY 'Interrupted by user.'
+   EXIT( ERROR.GEN_FAILURE)
+
+/* ----------------------------------------------------------------------- */
+/* Give a standard REXX error message and jump to the error line           */
+Error:
+   SAY 'REX'RIGHT( rc, 4, 0)': Error 'rc' running 'ThisFile', line 'sigl ||,
+   ': 'ERRORTEXT( rc)
+   EXIT( ERROR.GEN_FAILURE)
+
---
+++ src/netlabs/install/renudirs.cmd
@@ -0,0 +1,337 @@
+/****************************** Module Header *******************************
+*
+* Module Name: renudirs.cmd
+*
+* Syntax: renudirs [NEPMD]
+*
+* Helper batch for to rename some user's subdirectories
+*
+* These directories are named according to following scheme:
+*
+*    ex    -> ex_yyyy-mm-dd        (yyyy = year, mm = month, dd = day)
+*    mode  -> mode_yyyy-mm-dd      (long filename support required)
+*
+* If a directory name already exists, a counter is added:
+*
+*    ex_yyyy-mm-dd
+*    ex_yyyy-mm-dd_1
+*    ex_yyyy-mm-dd_2
+*
+* New empty directories were created on success.
+*
+* This is executed by the installer to ensure that a newly installed NEPMD
+* uses its own macros at first. Otherwise incompatibities may happen, because
+* EPM's behavior depends not only on the macros, but also on the library and
+* the configuration files. In the worst case, one would end up with a not
+* starting EPM, so renaming the files automatically is the best solution.
+*
+* After installation, merge your previous changes/additions with the newly
+* installed files of the netlabs tree.
+*
+* When no incompatibilies exist, the .ex files can be recreated by a
+* RecompileNew command, because the user's macro sources were not renamed.
+* For other files, and alternatively for the .ex files, the old user files
+* may be copied to the new empty directories.
+*
+* Copyright (c) Netlabs EPM Distribution Project 2002
+*
+* $Id: renudirs.cmd,v 1.1 2008-09-07 23:15:14 aschn Exp $
+*
+* ===========================================================================
+*
+* This file is part of the Netlabs EPM Distribution package and is free
+* software.  You can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software
+* Foundation, in version 2 as it comes in the "COPYING" file of the
+* Netlabs EPM Distribution.  This library is distributed in the hope that it
+* will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+****************************************************************************/
+/* Some header lines are used as help text */
+
+ /* initialize */
+ '@ECHO OFF';
+ call RxFuncAdd    'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs';
+ call SysLoadFuncs;
+ env = 'OS2ENVIRONMENT';
+ TRUE  = (1 = 1);
+ FALSE = (0 = 1);
+ Redirection = '>NUL 2>&1';
+ SIGNAL ON HALT NAME HALT
+ GlobalVars = 'env TRUE FALSE Redirection RootDir UserDir SubDirs' ||,
+              ' fQuiet ERROR.';
+
+ /* ###################### Configurable part starts ################### */
+ /* user subdirs to process */
+ SubDirs = 'ex mode'
+ /* ###################### Configurable part ends ##################### */
+
+ /* some OS/2 Error codes */
+ ERROR.NO_ERROR           =   0;
+ ERROR.INVALID_FUNCTION   =   1;
+ ERROR.FILE_NOT_FOUND     =   2;
+ ERROR.PATH_NOT_FOUND     =   3;
+ ERROR.ACCESS_DENIED      =   5;
+ ERROR.NOT_ENOUGH_MEMORY  =   8;
+ ERROR.INVALID_FORMAT     =  11;
+ ERROR.INVALID_DATA       =  13;
+ ERROR.NO_MORE_FILES      =  18;
+ ERROR.WRITE_FAULT        =  29;
+ ERROR.READ_FAULT         =  30;
+ ERROR.SHARING_VIOLATION  =  32;
+ ERROR.GEN_FAILURE        =  31;
+ ERROR.INVALID_PARAMETER  =  87;
+ ERROR.ENVVAR_NOT_FOUND   = 204;
+
+ /* INI app names and keys of NEPMD project from OS2.INI, defined in nepmd.h */
+ NEPMD_INI_APPNAME             = "NEPMD"
+ NEPMD_INI_KEYNAME_LANGUAGE    = "Language"
+ NEPMD_INI_KEYNAME_ROOTDIR     = "RootDir"
+ NEPMD_INI_KEYNAME_USERDIR     = "UserDir"
+ NEPMD_INI_KEYNAME_USERDIRNAME = "UserDirName"
+ NEPMD_INI_KEYNAME_USEHOME     = "UseHomeForUserDir"
+
+ /* default values of NEPMD installation */
+ UserDirName = 'myepm'
+ fUseHome    = FALSE;
+
+ /* defaults and further consts */
+ Help1StartLine  = 7
+ Help1EndLine    = 26
+ Help2StartLine  = 28
+ Help2EndLine    = 34
+ fQuiet = TRUE;
+ rc = ERROR.NO_ERROR;
+ ErrorQueueName = VALUE( 'NEPMD_RXQUEUE',, env);
+ ErrorMessage   = '';
+ RenamedDirs = ''
+
+ /* make sure we are called on purpose */
+ ARG Parm .;
+ IF Parm = 'NEPMD' THEN
+    fQuiet = TRUE;
+ ELSE
+    fQuiet = FALSE;
+
+ IF \fQuiet THEN
+ DO
+    SAY;
+    DO l = Help1StartLine TO Help1EndLine
+       SAY SUBSTR( SOURCELINE( l), 3);
+    END;
+    SAY;
+    SAY 'Do you want to continue? (Y/N)';
+    PULL Answer;
+    Answer = STRIP( Answer);
+    IF (ANSWER <> 'Y') THEN
+       EXIT( ERROR.GEN_FAILURE);
+ END;
+
+ /* get default dir values, depending on the path of this file */
+ PARSE Source . . CallName;
+ CallDir    = LEFT( CallName,   LASTPOS( '\', CallName) - 1);
+ NetlabsDir = LEFT( CallDir,    LASTPOS( '\', CallDir) - 1);
+ RootDir    = LEFT( NetlabsDir, LASTPOS( '\', NetlabsDir) - 1);
+ UserDir    = RootDir'\'UserDirName;
+
+ /* get dir values from OS2.INI */
+ DO UNTIL (TRUE)
+
+    /* get the base directory of the NEPMD installation */
+    PARSE VALUE SysIni( 'USER', NEPMD_INI_APPNAME, NEPMD_INI_KEYNAME_ROOTDIR) WITH RootDir'00'x;
+    IF (RootDir = 'ERROR:') THEN
+    DO
+       /* don't report error from here, use default values instead */
+       /*
+       rc = ERROR.PATH_NOT_FOUND;
+       ErrorMessage = 'Error: NEPMD configuration not found.';
+       */
+       LEAVE;
+    END;
+
+    /* get user directory */
+    UserDir = RootDir'\'UserDirName
+    DO 1
+       next = SysIni( 'USER', NEPMD_INI_APPNAME, NEPMD_INI_KEYNAME_USERDIR)
+       IF (next <> 'ERROR:') then
+       DO
+          next = STRIP( next, 't', '00'x)
+          IF (next > '') THEN
+          DO
+             UserDir = next
+             LEAVE
+          END
+       END
+
+       next = SysIni( 'USER', NEPMD_INI_APPNAME, NEPMD_INI_KEYNAME_USERDIRNAME)
+       IF (next <> 'ERROR:') then
+       DO
+          next = STRIP( next, 't', '00'x)
+          IF (next > '') THEN
+             UserDirName = next
+       END
+
+       next = SysIni( 'USER', NEPMD_INI_APPNAME, NEPMD_INI_KEYNAME_USEHOME)
+       IF (next <> 'ERROR:') then
+       DO
+          next = STRIP( next, 't', '00'x)
+          IF (next > '') THEN
+             fUseHome = next
+       END
+       IF (fUseHome) THEN
+       DO
+          Home = VALUE( 'HOME', , env)
+          IF Home > '' THEN
+          DO
+             rcx = SysFileTree Home, 'Found.', 'DO', '*+--*'  /* ADHRS */
+             IF (rcx = 0) THEN
+                IF (Found.0 > 0) THEN
+                DO
+                   UserDir = Home'\'UserDirName
+                   LEAVE
+                END
+          END
+       END
+
+    END
+
+    /* get date string */
+    IsoDate = GetIsoDate();
+
+    /* process dirs */
+    DO i = 1 to WORDS( SubDirs)
+       SubDir.i = WORD( SubDirs, i);
+       OldDir = UserDir'\'SubDir.i;
+
+       /* check for existing dir */
+       IF \DirExist( OldDir) THEN
+          ITERATE;
+
+       /* check for contained files */
+       IF \TreeContainsFiles( OldDir) THEN
+          ITERATE;
+
+       NewDirName = SubDir.i'_'IsoDate;
+       NewDir     = UserDir'\'NewDirName;
+
+       /* check for already existing dir name */
+       c = 0
+       DO WHILE DirExist( NewDir)
+          /* append counter to dir name */
+          c = c + 1
+          NewDirName = SubDir.i'_'IsoDate'_'c;
+          NewDir     = UserDir'\'NewDirName;
+       END;
+
+       /* rename dir */
+       IF \fQuiet THEN
+          SAY 'Renaming 'OldDir' -> 'NewDirName;
+       rc = RenameDir( OldDir, NewDirName);
+
+       /* recreate empty dirs on success */
+       IF (rc = 0) THEN
+       DO
+          rcx = SysMkDir( OldDir);
+          RenamedDirs = STRIP( RenamedDirs SubDir.i);
+       END;
+
+       /* check for error */
+       IF (rc <> 0) THEN
+          LEAVE;
+    END;
+
+ END;
+
+ /* report error (or success) message */
+ SELECT
+    /* no error here */
+    WHEN (rc = 0) THEN
+    DO
+       IF \fQuiet THEN
+       DO
+          IF (RenamedDirs = '') THEN
+          DO
+             SAY;
+             SAY 'No file contained, no directory renamed.';
+          END;
+          ELSE
+          DO
+             /* show rest of help text on success */
+             SAY;
+             DO l = Help2StartLine TO Help2EndLine
+                SAY SUBSTR( SOURCELINE( l), 3);
+             END;
+          END;
+       END;
+    END;
+
+    /* called by frame program: insert error */
+    /* message into standard REXX queue     */
+    WHEN (ErrorQueueName \= '') THEN
+    DO
+       rcx = RXQUEUE( 'SET', ErrorQueueName);
+       PUSH ErrorMessage;
+    END;
+
+    /* called directly, method */
+    OTHERWISE
+    DO
+       SAY ErrorMessage;
+       '@PAUSE';
+    END;
+ END;
+
+ EXIT( rc);
+
+/* ------------------------------------------------------------------------- */
+RenameDir: PROCEDURE
+ PARSE ARG OldDir, NewDirName;
+ rc = ERROR.NO_ERROR;
+
+ fSuccess = SysSetObjectData( OldDir, 'TITLE='NewDirName';');
+ IF fSuccess THEN
+    rc = 0;
+ ELSE
+    /* try the OS/2 command to get a useful rc */
+    'ren' OldDir NewDirName;
+
+ RETURN( rc);
+
+/* ------------------------------------------------------------------------- */
+GetIsoDate: PROCEDURE
+ PARSE VALUE DATE( 'S') WITH yyyy +4 mm +2 dd;
+
+ RETURN( yyyy'-'mm'-'dd);
+
+/* ------------------------------------------------------------------------- */
+FileExist: PROCEDURE
+ PARSE ARG FileName;
+
+ RETURN( STREAM( Filename, 'C', 'QUERY EXISTS') <> '');
+
+/* ------------------------------------------------------------------------- */
+DirExist: PROCEDURE
+ PARSE ARG DirName;
+
+ Found.0 = 0;
+ rcx = SysFileTree( DirName, 'Found.', 'DO');
+
+ RETURN( Found.0 > 0);
+
+/* ------------------------------------------------------------------------- */
+TreeContainsFiles: PROCEDURE
+ PARSE ARG DirName;
+
+ Found.0 = 0;
+ rcx = SysFileTree( DirName'\*', 'Found.', 'FOS');
+
+ RETURN( Found.0 > 0);
+
+/* ------------------------------------------------------------------------- */
+HALT:
+ SAY;
+ SAY 'Interrupted by user.';
+ EXIT( ERROR.GEN_FAILURE);
+
