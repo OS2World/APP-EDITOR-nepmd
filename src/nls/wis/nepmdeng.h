60a573ebceaf5d8020dad0f8253a638832df2995
aschn <aschn@72c63837-6175-e011-9ffd-00145e329d72>
2008-09-09 22:45:45 +0000

    - Reworked install files.

--- src/nls/wis/nepmdeng.h
+++ src/nls/wis/nepmdeng.h
@@ -2,11 +2,11 @@
 *
 * Module Name: nepmdeng.h
 *
-* Symbol definition file for nepmd.wis im english language
+* Symbol definition file for nepmd.wis in English language
 *
 * Copyright (c) Netlabs EPM Distribution Project 2002
 *
-* $Id: nepmdeng.h,v 1.13 2007-02-12 00:58:08 jbs Exp $
+* $Id: nepmdeng.h,v 1.14 2008-09-09 22:43:43 aschn Exp $
 *
 * ===========================================================================
 *
@@ -27,7 +27,7 @@
 #define PCK_2_TITLE "Custom icons for EPM"
 #define PCK_3_TITLE "Recompilation package"
 #define PCK_4_TITLE "Programming samples"
-#define PCK_5_TITLE "Speech Support (use with VoiceType only!)"
+#define PCK_5_TITLE "Speech Support (use with VoiceType only, now untested!)"
 #define PCK_99_TITLE "EPMBBS Base Application (use with OS/2 Warp 3 only!)"
 
 
@@ -35,22 +35,17 @@
 
 #define TITLE_NEPMD_EPM                          "EPM"
 #define TITLE_NEPMD_EPM_NEW_WINDOW               "EPM new window"
+#define TITLE_NEPMD_EPM_SHELL                    "EPM Shell"
+#define TITLE_NEPMD_EPM_TURBO                    "EPM turbo"
 
 #define TITLE_NEPMD_MORE_OBJECTS                 "More EPM objects"
+
+#define TITLE_STANDARD_EPM                       "Standard EPM"
 #define TITLE_NEPMD_EPM_E                        "EPM E"
 #define TITLE_NEPMD_EPM_EDIT_MACROFILE           "EPM edit E macrofile"
 #define TITLE_NEPMD_EPM_ERX                      "EPM EPM-REXX"
 #define TITLE_NEPMD_EPM_TEX                      "EPM TeX"
 #define TITLE_NEPMD_EPM_BIN                      "EPM Bin"
-
-#define TITLE_NEPMD_EPM_SHELL                    "EPM Shell"
-#define TITLE_NEPMD_EPM_TURBO                    "EPM turbo"
-#define TITLE_NEPMD_CHECK_USER_MACROS            "EPM - Check user macros"
-#define TITLE_NEPMD_RECOMPILE_NEW                "EPM - Recompile new user macros"
-#define TITLE_NEPMD_TOGGLE_CCVIEW                "EPM - Toggle new/same window"
-#define TITLE_NEPMD_CHANGE_STARTUPDIR            "EPM - Change startup dir"
-#define TITLE_NEPMD_TOGGLE_DEFASSOCS             "EPM - Toggle default assocs"
-#define TITLE_STANDARD_EPM                       "Standard EPM"
 
 #define TITLE_NEPMD_VIEW_NEUSR                   "NEPMD User's Guide"
 #define TITLE_NEPMD_VIEW_NEPRG                   "NEPMD Programming Guide"
@@ -58,12 +53,9 @@
 #define TITLE_NEPMD_VIEW_EPMTECH                 "EPMBBS Technical Reference"
 #define TITLE_NEPMD_VIEW_EPMHELP                 "EPM Help"
 
-#define TITLE_NEPMD_RECOMP                       "Recompile EPM.E"
 #define TITLE_NEPMD_NEPMD_SAMPLES_FOLDER         "EPMBBS Programming Samples"
 #define TITLE_NEPMD_NEPMD_VIEW_EPMTK             "Programmers Guide to the EPM Toolkit"
 #define TITLE_NEPMD_NEPMD_VIEW_EPMCM_README      "EPM C macro Readme"
 #define TITLE_NEPMD_NEPMD_VIEW_EPMRM_README      "EPM REXX macro Readme"
 #define TITLE_NEPMD_NEPMD_SPEECH                 "Start EPM Speech Support"
-#define TITLE_NEPMD_NEPMD_SPEECH_SHADOW          "Start EPM Speech Support"
-#define TITLE_NEPMD_NEPMD_WPSTART_SHADOW         "Startup Folder Shadow"
 
